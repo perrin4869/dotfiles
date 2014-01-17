6610d37cd4d85eb31b39245fb962bf65c92d293b
Julian Grinblat <julian@dotcore.co.il>
2014-01-18 03:10:15 +0900

    Created a gitignore_global file

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles # dotfiles directory
 olddir=~/dotfiles_old # old dotfiles backup directory
-files="bashrc git-completion.bash gitconfig vimrc gvimrc vimsauce vim tmux.conf bash_profile nzbget screenrc cmus" # list of files/folders to symlink in homedir
+files="bashrc git-completion.bash gitconfig vimrc gvimrc gitignore_global vim tmux.conf bash_profile nzbget screenrc cmus" # list of files/folders to symlink in homedir
 
 ##########
 

ddae07387079ee58f584caa3dc154240bbb39255
Julian Grinblat <julian@dotcore.co.il>
2021-06-20 18:30:37 +0900

    Fix screensaver.lua

--- config/mpv/scripts/screensaver.lua
+++ config/mpv/scripts/screensaver.lua
@@ -1,6 +1,14 @@
 -- since 0.33 this is not used by default
 -- unfortunately --stop-screensaver does not seem to work on i3wm
 local utils = require 'mp.utils'
-mp.add_periodic_timer(30, function()
+local timer = mp.add_periodic_timer(30, function()
     utils.subprocess({args={"xdg-screensaver", "reset"}})
+end)
+
+mp.observe_property("pause", "bool", function(name, value)
+    if value == true then
+      timer:kill() -- stop will remember the elapsed time
+    else
+      timer:resume()
+    end
 end)

e319e13e52c6c80f57f946f83823cd3e7d0e3aa0
Julian Grinblat <julian@dotcore.co.il>
2021-08-12 14:23:22 +0900

    Add nvim code_lens only when supported by the lsp

--- home/.config/nvim/lua/lsp.lua
+++ home/.config/nvim/lua/lsp.lua
@@ -46,11 +46,13 @@ local on_attach = function(client, bufnr)
   buf_set_keymap('n', '<leader>ds', '<cmd>lua vim.lsp.buf.document_symbol()<CR>', opts)
   buf_set_keymap('n', '<leader>ws', '<cmd>lua vim.lsp.buf.workspace_symbol()<CR>', opts)
 
-  vim.cmd([[aug lsp_codelens]])
-  vim.cmd([[autocmd! * <buffer=]]..tostring(bufnr)..[[>]])
-  vim.cmd([[autocmd BufEnter,CursorHold,InsertLeave <buffer=]]..tostring(bufnr)..[[> lua vim.lsp.codelens.refresh()]])
-  vim.cmd([[aug END]])
-  buf_set_keymap("n", "<leader>cl", "<Cmd>lua vim.lsp.codelens.run()<CR>", opts)
+  if client ~= nil and client.resolved_capabilities.code_lens then
+    vim.cmd([[aug lsp_codelens]])
+    vim.cmd([[autocmd! * <buffer=]]..tostring(bufnr)..[[>]])
+    vim.cmd([[autocmd BufEnter,CursorHold,InsertLeave <buffer=]]..tostring(bufnr)..[[> lua vim.lsp.codelens.refresh()]])
+    vim.cmd([[aug END]])
+    buf_set_keymap("n", "<leader>cl", "<Cmd>lua vim.lsp.codelens.run()<CR>", opts)
+  end
 
   buf_set_keymap("n", "<leader>xx", "<cmd>Trouble<cr>", opts)
   buf_set_keymap("n", "<leader>xw", "<cmd>Trouble lsp_workspace_diagnostics<cr>", opts)

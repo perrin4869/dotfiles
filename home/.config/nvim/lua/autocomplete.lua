00b42a0fb960ce48cb17181d6ef049b7cca4beb6
Julian Grinblat <julian@dotcore.co.il>
2021-10-18 17:48:42 +0900

    nvim-cmp ConfirmBehavior.Replace

--- home/.config/nvim/lua/autocomplete.lua
+++ home/.config/nvim/lua/autocomplete.lua
@@ -12,7 +12,7 @@ cmp.setup {
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.close(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<CR>'] = cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Replace }),
   },
   formatting = {
     format = lspkind.cmp_format({with_text = false})

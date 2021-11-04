c78e51ce609196dcebb2b2dfe5fcb4a26345a4b1
Julian Grinblat <julian@dotcore.co.il>
2021-11-04 21:34:38 +0900

    Fix conflicts betweer telescope and hop.nvim mappings

--- home/.config/nvim/lua/finder.lua
+++ home/.config/nvim/lua/finder.lua
@@ -16,13 +16,14 @@ local opts = { noremap=true, silent=true }
 
 keymap('n', '<C-p>', [[<cmd>lua require('telescope.builtin').find_files()<CR>]], opts)
 
---Add leader shortcuts
-keymap('n', '<leader><space>', [[<cmd>lua require('telescope.builtin').buffers()<CR>]], opts)
-keymap('n', '<leader>sf', [[<cmd>lua require('telescope.builtin').find_files({previewer = false})<CR>]], opts)
-keymap('n', '<leader>sb', [[<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<CR>]], opts)
-keymap('n', '<leader>sh', [[<cmd>lua require('telescope.builtin').help_tags()<CR>]], opts)
-keymap('n', '<leader>st', [[<cmd>lua require('telescope.builtin').tags()<CR>]], opts)
-keymap('n', '<leader>sd', [[<cmd>lua require('telescope.builtin').grep_string()<CR>]], opts)
-keymap('n', '<leader>sp', [[<cmd>lua require('telescope.builtin').live_grep()<CR>]], opts)
-keymap('n', '<leader>so', [[<cmd>lua require('telescope.builtin').tags{ only_current_buffer = true }<CR>]], opts)
-keymap('n', '<leader>?', [[<cmd>lua require('telescope.builtin').oldfiles()<CR>]], opts)
+-- nvim-lua/kickstart.nvim uses <leader>s, but it conflicts with hop.nvim
+-- so here the prefix is gs
+keymap('n', 'g<space>', [[<cmd>lua require('telescope.builtin').buffers()<CR>]], opts)
+keymap('n', 'gsf', [[<cmd>lua require('telescope.builtin').find_files({previewer = false})<CR>]], opts)
+keymap('n', 'gsb', [[<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<CR>]], opts)
+keymap('n', 'gsh', [[<cmd>lua require('telescope.builtin').help_tags()<CR>]], opts)
+keymap('n', 'gst', [[<cmd>lua require('telescope.builtin').tags()<CR>]], opts)
+keymap('n', 'gsd', [[<cmd>lua require('telescope.builtin').grep_string()<CR>]], opts)
+keymap('n', 'gsp', [[<cmd>lua require('telescope.builtin').live_grep()<CR>]], opts)
+keymap('n', 'gso', [[<cmd>lua require('telescope.builtin').tags{ only_current_buffer = true }<CR>]], opts)
+keymap('n', 'g?', [[<cmd>lua require('telescope.builtin').oldfiles()<CR>]], opts)

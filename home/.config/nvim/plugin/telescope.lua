9f93862f60fc2eca0e7cc5edf799b5bf36fef58a
Julian Grinblat <julian@dotcore.co.il>
2021-11-14 13:08:25 +0900

    Add project_files

--- home/.config/nvim/plugin/telescope.lua
+++ home/.config/nvim/plugin/telescope.lua
@@ -10,16 +10,23 @@ require('telescope').setup {
 require('telescope').load_extension('fzf')
 require('telescope').load_extension('dap')
 
+local project_files = function()
+  local opts = {} -- define here if you want to define something
+  local ok = pcall(require'telescope.builtin'.git_files, opts)
+  if not ok then require'telescope.builtin'.find_files(opts) end
+end
+
 -- Mappings.
 local keymap = vim.api.nvim_set_keymap
 local opts = { noremap=true, silent=true }
 
-keymap('n', '<C-p>', [[<cmd>lua require('telescope.builtin').find_files()<CR>]], opts)
+keymap('n', '<C-p>', [[<cmd>lua require('utils').project_files()<CR>]], opts)
 
 -- nvim-lua/kickstart.nvim uses <leader>s, but it conflicts with hop.nvim
 -- so here the prefix is gs
 keymap('n', 'g<space>', [[<cmd>lua require('telescope.builtin').buffers()<CR>]], opts)
-keymap('n', 'gsf', [[<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<CR>]], opts)
+keymap('n', 'gsf', [[<cmd>lua require('telescope.builtin').find_files()<CR>]], opts)
+keymap('n', 'gsj', [[<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<CR>]], opts)
 keymap('n', 'gsh', [[<cmd>lua require('telescope.builtin').help_tags()<CR>]], opts)
 keymap('n', 'gst', [[<cmd>lua require('telescope.builtin').tags()<CR>]], opts)
 keymap('n', 'gsd', [[<cmd>lua require('telescope.builtin').grep_string()<CR>]], opts)

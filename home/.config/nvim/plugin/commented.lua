afdb279163b5a5ce02a85a3951643d7effe4b6a1
Julian Grinblat <julian@dotcore.co.il>
2021-12-03 15:20:21 +0900

    Replace commented.nvim with vim-commentary

--- home/.config/nvim/plugin/commented.lua
+++
@@ -1,6 +0,0 @@
-require("commented").setup({
-  	keybindings = {n = "gc", v = "gc", nl = "gcc"},
-    hooks = {
-        before_comment = require("ts_context_commentstring.internal").update_commentstring,
-    },
-})

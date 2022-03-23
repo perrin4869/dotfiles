141749c28fa72ea860eac7132cc72decf17da693
Julian Grinblat <julian@dotcore.co.il>
2022-03-23 17:36:38 +0900

    Add option to disable mpv screensaver inhibitor

--- home/.config/mpv/scripts/screensaver.lua
+++ home/.config/mpv/scripts/screensaver.lua
@@ -1,14 +1,22 @@
 -- since 0.33 this is not used by default
 -- unfortunately --stop-screensaver does not seem to work on i3wm
-local utils = require 'mp.utils'
-local timer = mp.add_periodic_timer(30, function()
-    utils.subprocess({args={"xdg-screensaver", "reset"}})
-end)
+require 'mp.options'
+local options = {
+    enabled = true,
+}
+read_options(options, "inhibit")
 
-mp.observe_property("pause", "bool", function(name, value)
-    if value == true then
-      timer:kill() -- stop will remember the elapsed time
-    else
-      timer:resume()
-    end
-end)
+if options.enabled then
+  local utils = require 'mp.utils'
+  local timer = mp.add_periodic_timer(30, function()
+      utils.subprocess({args={"xdg-screensaver", "reset"}})
+  end)
+
+  mp.observe_property("pause", "bool", function(name, value)
+      if value == true then
+        timer:kill() -- stop will remember the elapsed time
+      else
+        timer:resume()
+      end
+  end)
+end

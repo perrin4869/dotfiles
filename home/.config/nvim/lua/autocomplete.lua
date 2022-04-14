1f249b501f806ec51cdf02c037b43181c85628d6
Julian Grinblat <julian@dotcore.co.il>
2022-04-14 18:26:34 +0900

    Upgrade submodules

--- home/.config/nvim/lua/autocomplete.lua
+++ home/.config/nvim/lua/autocomplete.lua
@@ -7,13 +7,13 @@ cmp.setup {
       vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` user.
     end,
   },
-  mapping = {
+  mapping = cmp.mapping.preset.insert({
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.close(),
     ['<CR>'] = cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Replace }),
-  },
+  }),
   formatting = {
     format = lspkind.cmp_format({with_text = false})
   },

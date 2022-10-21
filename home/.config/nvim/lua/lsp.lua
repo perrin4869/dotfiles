58323415b4e975536dbebe0d7c08706ee66abe4b
Julian Grinblat <julian@dotcore.co.il>
2022-10-21 13:18:50 +0900

    Update nvim-cmp to new default_capabilities api

--- home/.config/nvim/lua/lsp.lua
+++ home/.config/nvim/lua/lsp.lua
@@ -129,7 +129,7 @@ status.register_progress()
 
 require("trouble").setup {}
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities();
 
 config.vimls.setup{on_attach=on_attach, capabilities=capabilities}
 config.tsserver.setup{on_attach=on_attach, capabilities=capabilities}

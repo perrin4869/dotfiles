b557a99fcee7465269e3a9bd5179e1adc9c980a9
Julian Grinblat <julian@dotcore.co.il>
2022-11-25 13:43:30 +0900

    refactor: use LspAttach on metals and typescript.lua

--- home/.config/nvim/after/ftplugin/scala.lua
+++ home/.config/nvim/after/ftplugin/scala.lua
@@ -1,13 +1,29 @@
 local metals = require"metals"
 
-local opts = { noremap=true, silent=true }
+vim.api.nvim_create_augroup("LspAttach_metals", {})
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = "LspAttach_metals",
+  callback = function(args)
+    if not (args.data and args.data.client_id) then
+      return
+    end
 
--- Toggle panel with Tree Views
-vim.keymap.set('n', '<leader>tv', metals.toggle_tree_view, opts)
--- Reveal current current class (trait or object) in Tree View 'metalsPackages'
-vim.keymap.set('n', '<leader>tf', metals.reveal_in_tree, opts)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if (client.name ~= "metals") then
+      return
+    end
 
-vim.keymap.set('n', '<leader>o', metals.organize_imports, opts)
-vim.api.nvim_buf_create_user_command(0, 'OR', 'MetalsOrganizeImport', { nargs = 0 })
+    local bufnum = args.buf
+    local opts = { noremap=true, silent=true, buffer=bufnum }
+
+    -- Toggle panel with Tree Views
+    vim.keymap.set('n', '<leader>tv', metals.toggle_tree_view, opts)
+    -- Reveal current current class (trait or object) in Tree View 'metalsPackages'
+    vim.keymap.set('n', '<leader>tf', metals.reveal_in_tree, opts)
+
+    vim.keymap.set('n', '<leader>o', metals.organize_imports, opts)
+    vim.api.nvim_buf_create_user_command(bufnum, 'OR', 'MetalsOrganizeImport', { nargs = 0 })
+  end,
+})
 
 require("lsp").initialize_metals()
--- home/.config/nvim/after/ftplugin/typescript.lua
+++ home/.config/nvim/after/ftplugin/typescript.lua
@@ -1,2 +1,22 @@
-vim.keymap.set('n', '<leader>o', require("typescript").actions.organizeImports, { noremap=true, silent=true })
-vim.api.nvim_buf_create_user_command(0, 'OR', 'TypescriptOrganizeImport', { nargs = 0 })
+local typescript = require("typescript")
+
+vim.api.nvim_create_augroup("LspAttach_typescript", {})
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = "LspAttach_typescript",
+  callback = function(args)
+    if not (args.data and args.data.client_id) then
+      return
+    end
+
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if (client.name ~= "tsserver") then
+      return
+    end
+
+    local bufnum = args.buf
+    local opts = { noremap=true, silent=true, buffer=bufnum }
+
+    vim.keymap.set('n', '<leader>o', typescript.actions.organizeImports, opts)
+    vim.api.nvim_buf_create_user_command(bufnum, 'OR', 'TypescriptOrganizeImport', { nargs = 0 })
+  end,
+})

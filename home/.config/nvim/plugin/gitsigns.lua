bf6f2605dfab98707f604abfc92e16b7a7834f47
Julian Grinblat <julian@dotcore.co.il>
2023-02-09 15:46:55 +0900

    feat(neovim): add hunk keymap desc

--- home/.config/nvim/plugin/gitsigns.lua
+++ home/.config/nvim/plugin/gitsigns.lua
@@ -13,13 +13,13 @@ require'gitsigns'.setup{
       if vim.wo.diff then return ']c' end
       vim.schedule(function() gs.next_hunk() end)
       return '<Ignore>'
-    end, {expr=true})
+    end, {expr=true,desc="next_hunk"})
 
     map('n', '[c', function()
       if vim.wo.diff then return '[c' end
       vim.schedule(function() gs.prev_hunk() end)
       return '<Ignore>'
-    end, {expr=true})
+    end, {expr=true,desc="prev_thunk"})
 
     -- Actions
     map({'n', 'v'}, '<leader>hs', ':Gitsigns stage_hunk<CR>')

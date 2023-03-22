be01f544f6f1fcd7eef7aeb028eb7b427740f241
Julian Grinblat <julian@dotcore.co.il>
2023-03-22 19:09:20 +0900

    chore: migrate nvim-ts-rainbow to nvim-ts-rainbow2

--- home/.config/nvim/plugin/treesitter.lua
+++ home/.config/nvim/plugin/treesitter.lua
@@ -42,7 +42,13 @@ require'nvim-treesitter.configs'.setup {
     updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
     persist_queries = false, -- Whether the query persists across vim sessions
   },
-  rainbow = {enable = true, extended_mode = true},
+  rainbow = {
+    enable = true,
+    query = {
+      'rainbow-parens',
+      html = 'rainbow-tags'
+    },
+  },
   textobjects = {
     select = {
       enable = true,

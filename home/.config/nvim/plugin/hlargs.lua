38ac4e74fb757992935316433983bffd9c9155f8
Julian Grinblat <julian@dotcore.co.il>
2023-06-14 13:52:47 +0900

    feat(nvim): add hlargs

---
+++ home/.config/nvim/plugin/hlargs.lua
@@ -0,0 +1,44 @@
+local hlargs = require("hlargs")
+
+hlargs.setup {
+  disable = function(_, bufnr)
+    if vim.b[bufnr].semantic_tokens then
+      return true
+    end
+
+    local clients = vim.lsp.get_active_clients { bufnr = bufnr }
+    for _, c in pairs(clients) do
+      local caps = c.server_capabilities
+      if caps.semanticTokensProvider and caps.semanticTokensProvider.full then
+        -- cache languages that were already resolved
+        vim.b[bufnr].semantic_tokens = true
+        return vim.b[bufnr].semantic_tokens
+      end
+    end
+  end
+}
+
+vim.api.nvim_create_augroup("LspAttach_hlargs", {})
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = "LspAttach_hlargs",
+  callback = function(args)
+    local bufnr = args.buf
+    if vim.b[bufnr].semantic_tokens then
+      return
+    end
+
+    if not (args.data and args.data.client_id) then
+      return
+    end
+
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    local caps = client.server_capabilities
+    if caps.semanticTokensProvider and caps.semanticTokensProvider.full then
+      vim.b[bufnr].semantic_tokens = true
+    end
+
+    -- TODO: disable only on the buffer that was attached
+    -- https://github.com/m-demare/hlargs.nvim/issues/69
+    hlargs.disable()
+  end,
+})

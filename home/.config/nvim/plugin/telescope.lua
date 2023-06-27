67352d750d848314aacf41d3553959680ef65685
Julian Grinblat <julian@dotcore.co.il>
2023-06-27 22:08:18 +0900

    feat(vim): add vim-caser

--- home/.config/nvim/plugin/telescope.lua
+++ home/.config/nvim/plugin/telescope.lua
@@ -29,23 +29,23 @@ local get_opts = utils.create_get_opts(opts)
 vim.keymap.set('n', '<C-p>', project_files, opts)
 
 -- nvim-lua/kickstart.nvim uses <leader>s, but it conflicts with hop.nvim
--- so here the prefix is gs
-vim.keymap.set('n', 'gsf', builtin.find_files, get_opts({ desc="telescope.find_files" }))
-vim.keymap.set('n', 'gsj', builtin.current_buffer_fuzzy_find, get_opts({ desc="telescope.find_files" }))
-vim.keymap.set('n', 'gsh', builtin.help_tags, get_opts({ desc="telescope.help_tags" }))
-vim.keymap.set('n', 'gst', builtin.tags, get_opts({ desc="telescope.tags" }))
-vim.keymap.set('n', 'gsd', builtin.grep_string, get_opts({ desc="telescope.grep_string" }))
-vim.keymap.set('n', 'gsp', builtin.live_grep, get_opts({ desc="telescope.live_grep" }))
-vim.keymap.set('n', 'gsk', builtin.keymaps, get_opts({ desc="telescope.keymaps" }))
-vim.keymap.set('n', 'gso', function() builtin.tags{ only_current_buffer = true } end,
+-- so here the prefix is gt
+vim.keymap.set('n', 'gtf', builtin.find_files, get_opts({ desc="telescope.find_files" }))
+vim.keymap.set('n', 'gtj', builtin.current_buffer_fuzzy_find, get_opts({ desc="telescope.find_files" }))
+vim.keymap.set('n', 'gth', builtin.help_tags, get_opts({ desc="telescope.help_tags" }))
+vim.keymap.set('n', 'gtt', builtin.tags, get_opts({ desc="telescope.tags" }))
+vim.keymap.set('n', 'gtd', builtin.grep_string, get_opts({ desc="telescope.grep_string" }))
+vim.keymap.set('n', 'gtp', builtin.live_grep, get_opts({ desc="telescope.live_grep" }))
+vim.keymap.set('n', 'gtk', builtin.keymaps, get_opts({ desc="telescope.keymaps" }))
+vim.keymap.set('n', 'gto', function() builtin.tags{ only_current_buffer = true } end,
   get_opts({ desc="telescope.tags" }))
 vim.keymap.set('n', 'gb', builtin.buffers, get_opts({ desc="telescope.buffers" }))
 vim.keymap.set('n', 'g?', builtin.oldfiles, get_opts({ desc="telescope.oldfiles" }))
 
-vim.keymap.set('n', 'gsc', telescope.extensions.neoclip.default, get_opts({ desc="telescope.neoclip" }))
-vim.keymap.set('n', 'gst', telescope.extensions.file_browser.file_browser, get_opts({ desc="telescope.file_browser" }))
+vim.keymap.set('n', 'gtc', telescope.extensions.neoclip.default, get_opts({ desc="telescope.neoclip" }))
+vim.keymap.set('n', 'gtt', telescope.extensions.file_browser.file_browser, get_opts({ desc="telescope.file_browser" }))
 
 -- git
-vim.keymap.set('n', 'gsb', builtin.git_branches, get_opts({ desc="telescope.git_branches" }))
-vim.keymap.set('n', 'gsg', builtin.git_commits, get_opts({ desc="telescope.git_commits" }))
-vim.keymap.set('n', 'gss', builtin.git_status, get_opts({ desc="telescope.git_status" }))
+vim.keymap.set('n', 'gtb', builtin.git_branches, get_opts({ desc="telescope.git_branches" }))
+vim.keymap.set('n', 'gtg', builtin.git_commits, get_opts({ desc="telescope.git_commits" }))
+vim.keymap.set('n', 'gts', builtin.git_status, get_opts({ desc="telescope.git_status" }))

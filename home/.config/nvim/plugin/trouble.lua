d5c8b81c2b1eabae30ff87e50898bea5227e7737
Julian Grinblat <julian@dotcore.co.il>
2023-07-03 17:07:34 +0900

    fix: allow some trouble.nvim mappings on buffers without lsp clients attached

--- home/.config/nvim/plugin/trouble.lua
+++ home/.config/nvim/plugin/trouble.lua
@@ -14,16 +14,18 @@ vim.api.nvim_create_autocmd("LspAttach", {
     local opts = { silent=true, buffer=args.buf }
     local get_opts = utils.create_get_opts(opts)
 
-    vim.keymap.set("n", "<leader>xx", trouble.toggle, get_opts({ desc="trouble.toggle" }))
-    vim.keymap.set("n", "<leader>xw", function() trouble.toggle('workspace_diagnostics') end,
-      get_opts({ desc="trouble.workspace_diagnostics" }))
     vim.keymap.set("n", "<leader>xd", function() trouble.toggle('document_diagnostics') end,
       get_opts({ desc="trouble.document_diagnostics" }))
-    vim.keymap.set("n", "<leader>xl", function() trouble.toggle('loclist') end,
-      get_opts({ desc="trouble.loclist" }))
-    vim.keymap.set("n", "<leader>xq", function() trouble.toggle('quickfix') end,
-      get_opts({ desc="trouble.quickfix" }))
     vim.keymap.set("n", "gR", function() trouble.toggle('lsp_references') end,
       get_opts({ desc="trouble.lsp_references" }))
   end,
 })
+
+local get_opts = utils.create_get_opts({ silent = true })
+vim.keymap.set("n", "<leader>xx", trouble.toggle, get_opts({ desc="trouble.toggle" }))
+vim.keymap.set("n", "<leader>xw", function() trouble.toggle('workspace_diagnostics') end,
+  get_opts({ desc="trouble.workspace_diagnostics" }))
+vim.keymap.set("n", "<leader>xl", function() trouble.toggle('loclist') end,
+  get_opts({ desc="trouble.loclist" }))
+vim.keymap.set("n", "<leader>xq", function() trouble.toggle('quickfix') end,
+  get_opts({ desc="trouble.quickfix" }))

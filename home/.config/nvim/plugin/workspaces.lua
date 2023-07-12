688bfb405c3c2ee7ac033e2402d84082c4407aa1
Julian Grinblat <julian@dotcore.co.il>
2023-07-13 04:14:46 +0900

    feat(neovim): add persistence.nvim

--- home/.config/nvim/plugin/workspaces.lua
+++ home/.config/nvim/plugin/workspaces.lua
@@ -1,7 +1,20 @@
 local project_files = require('project_files')
+local persistence = require('persistence')
 
-require("workspaces").setup{
+local function toggle_tree()
+  require'nvim-tree.api'.tree.toggle{focus=false}
+end
+
+require('workspaces').setup{
   hooks = {
-    open = { "NvimTreeOpen", project_files },
+    open = function ()
+      if (vim.fn.filereadable(persistence.get_current()) ~= 0) then
+        persistence.load()
+        toggle_tree()
+      else
+        toggle_tree()
+        project_files()
+      end
+    end,
   }
 }

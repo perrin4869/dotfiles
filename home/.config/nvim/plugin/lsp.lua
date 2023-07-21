3a48abb7ae3a36109d0339987a6ab619019b074d
Julian Grinblat <julian@dotcore.co.il>
2023-07-21 19:52:31 +0900

    chore(neovim): tweak lsp mappings

--- home/.config/nvim/plugin/lsp.lua
+++ home/.config/nvim/plugin/lsp.lua
@@ -23,13 +23,14 @@ vim.api.nvim_create_autocmd("LspAttach", {
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, get_opts({ desc="lsp.definition" }))
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, get_opts({ desc="lsp.hover" }))
     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, get_opts({ desc="lsp.implementation" }))
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, get_opts({ desc="lsp.signature_help" }))
+    vim.keymap.set('n', 'gk', vim.lsp.buf.signature_help, get_opts({ desc="lsp.signature_help" }))
+    vim.keymap.set('i', '<C-k>', vim.lsp.buf.signature_help, get_opts({ desc="lsp.signature_help" }))
     vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, get_opts({ desc="lsp.add_workspace_folder" }))
     vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder,
       get_opts({ desc="lsp.remove_workspace_folder" }))
     vim.keymap.set('n', '<leader>wl', function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end,
       get_opts({ desc="lsp.list_workspace_folders" }))
-    vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, get_opts({ desc="lsp.type_definition" }))
+    vim.keymap.set('n', 'gy', vim.lsp.buf.type_definition, get_opts({ desc="lsp.type_definition" }))
     vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, get_opts({ desc="lsp.rename" }))
     vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, get_opts({ desc="lsp.code_action" }))
     vim.keymap.set('n', 'gr', vim.lsp.buf.references, get_opts({ desc="lsp.references" }))

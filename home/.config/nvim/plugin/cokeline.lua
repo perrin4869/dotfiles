49a5b417c48bcb607c794806089ecef53b753eaf
Julian Grinblat <julian@dotcore.co.il>
2023-07-24 00:19:08 +0900

    feat(nvim): improve cokeline

--- home/.config/nvim/plugin/cokeline.lua
+++ home/.config/nvim/plugin/cokeline.lua
@@ -1,4 +1,9 @@
+local is_picking_focus = require('cokeline/mappings').is_picking_focus
+local is_picking_close = require('cokeline/mappings').is_picking_close
 local get_hex = require('cokeline/utils').get_hex
+
+local red = vim.g.terminal_color_1
+local yellow = vim.g.terminal_color_3
 
 require('cokeline').setup({
   default_hl = {
@@ -17,12 +22,36 @@ require('cokeline').setup({
       fg = get_hex('Normal', 'fg')
     },
     {
-      text = function(buffer) return '    ' .. buffer.devicon.icon end,
-      fg = function(buffer) return buffer.devicon.color end,
+      text = function(buffer)
+        return '    ' ..
+          ((is_picking_focus() or is_picking_close())
+          and buffer.pick_letter .. ' '
+          or buffer.devicon.icon)
+      end,
+      fg = function(buffer)
+        return
+          (is_picking_focus() and yellow)
+          or (is_picking_close() and red)
+          or buffer.devicon.color
+      end,
+      style = function(_)
+        return
+          (is_picking_focus() or is_picking_close())
+          and 'italic,bold'
+          or nil
+      end,
     },
     {
-      text = function(buffer) return buffer.filename .. '    ' end,
+      text = function(buffer) return buffer.filename end,
       style = function(buffer) return buffer.is_focused and 'bold' or nil end,
+    },
+    {
+      text = function(buffer)
+        return buffer.is_modified and ' ' or ''
+      end,
+    },
+    {
+      text = '    '
     },
     {
       text = '󰅖',
@@ -33,3 +62,5 @@ require('cokeline').setup({
     },
   },
 })
+
+vim.keymap.set('n', '<leader>bp', '<Plug>(cokeline-pick-focus)', {silent=true,noremap=true})

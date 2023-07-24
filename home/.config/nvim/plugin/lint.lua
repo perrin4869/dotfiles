23ad6d19ca65dcc4f1f568766d1248029b960d4c
Julian Grinblat <julian@dotcore.co.il>
2023-07-24 22:21:09 +0900

    feat(neovim): replace ale with neovim variants (#2254)

---
+++ home/.config/nvim/plugin/lint.lua
@@ -0,0 +1,14 @@
+require("lint").linters_by_ft = {
+	lua = { "luacheck" },
+	css = { "stylelint" },
+	javascript = { "eslint_d" },
+	javascriptreact = { "eslint_d" },
+	typescript = { "eslint_d" },
+	typescriptreact = { "eslint_d" },
+}
+
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "TextChanged" }, {
+	callback = function()
+		require("lint").try_lint()
+	end,
+})

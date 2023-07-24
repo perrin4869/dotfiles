b7b5fa1ee38e34dadf277e44849eba558a70b7ef
Julian Grinblat <julian@dotcore.co.il>
2023-07-24 16:49:20 +0900

    fix(neovim): typescript-tools migration complete

--- home/.config/nvim/after/ftplugin/typescript.lua
+++ home/.config/nvim/after/ftplugin/typescript.lua
@@ -1,4 +1,5 @@
-local typescript = require("typescript")
+local c = require "typescript-tools.protocol.constants"
+local api = require "typescript-tools.api"
 
 vim.api.nvim_create_autocmd("LspAttach", {
   group = vim.api.nvim_create_augroup("LspAttach_typescript", {}),
@@ -8,14 +9,14 @@ vim.api.nvim_create_autocmd("LspAttach", {
     end
 
     local client = vim.lsp.get_client_by_id(args.data.client_id)
-    if (client.name ~= "tsserver") then
+    if (client == nil or client.name ~= "typescript-tools") then
       return
     end
 
     local bufnum = args.buf
     local opts = { noremap=true, silent=true, buffer=bufnum }
 
-    vim.keymap.set('n', '<leader>o', typescript.actions.organizeImports, opts)
-    vim.api.nvim_buf_create_user_command(bufnum, 'OR', 'TypescriptOrganizeImport', { nargs = 0 })
+    vim.keymap.set('n', '<leader>o', function () api.organize_imports(c.OrganizeImportsMode.All) end, opts)
+    vim.api.nvim_buf_create_user_command(bufnum, 'OR', 'TSToolsOrganizeImports', { nargs = 0 })
   end,
 })

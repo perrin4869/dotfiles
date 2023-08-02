c7280111b0033fcc887558dd93f7bad99152ed81
Julian Grinblat <julian@dotcore.co.il>
2023-08-03 02:10:06 +0900

    feat(neovim/autopairs): use better version of spaces between parentheses

--- home/.config/nvim/plugin/autopairs.lua
+++ home/.config/nvim/plugin/autopairs.lua
@@ -1,5 +1,6 @@
 local npairs = require("nvim-autopairs")
 local Rule = require("nvim-autopairs.rule")
+local cond = require("nvim-autopairs.conds")
 
 npairs.setup({
 	check_ts = true,
@@ -18,24 +19,34 @@ npairs.setup({
 
 local brackets = { { "(", ")" }, { "[", "]" }, { "{", "}" } }
 npairs.add_rules({
-	Rule(" ", " "):with_pair(function(opts)
-		local pair = opts.line:sub(opts.col - 1, opts.col)
-		return vim.tbl_contains({
-			brackets[1][1] .. brackets[1][2],
-			brackets[2][1] .. brackets[2][2],
-			brackets[3][1] .. brackets[3][2],
-		}, pair)
-	end),
+	Rule(" ", " ")
+		:with_pair(function(opts)
+			local pair = opts.line:sub(opts.col - 1, opts.col)
+			return vim.tbl_contains({
+				brackets[1][1] .. brackets[1][2],
+				brackets[2][1] .. brackets[2][2],
+				brackets[3][1] .. brackets[3][2],
+			}, pair)
+		end)
+		:with_move(cond.none())
+		:with_cr(cond.none())
+		:with_del(function(opts)
+			local col = vim.api.nvim_win_get_cursor(0)[2]
+			local context = opts.line:sub(col - 1, col + 2)
+			return vim.tbl_contains({
+				brackets[1][1] .. "  " .. brackets[1][2],
+				brackets[2][1] .. "  " .. brackets[2][2],
+				brackets[3][1] .. "  " .. brackets[3][2],
+			}, context)
+		end),
 })
 for _, bracket in pairs(brackets) do
-	npairs.add_rules({
-		Rule(bracket[1] .. " ", " " .. bracket[2])
-			:with_pair(function()
-				return false
-			end)
-			:with_move(function(opts)
-				return opts.prev_char:match(".%" .. bracket[2]) ~= nil
-			end)
-			:use_key(bracket[2]),
-	})
+	Rule("", " " .. bracket[2])
+		:with_pair(cond.none())
+		:with_move(function(opts)
+			return opts.char == bracket[2]
+		end)
+		:with_cr(cond.none())
+		:with_del(cond.none())
+		:use_key(bracket[2])
 end

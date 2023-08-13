cbecefa50a6cdca56671632c0a91e3ec0e3e22a3
Julian Grinblat <julian@dotcore.co.il>
2023-08-13 17:36:47 +0900

    feat(neovim): replace custom autoformat function with lsp-format.nvim

--- home/.config/nvim/after/ftplugin/scala.lua
+++ home/.config/nvim/after/ftplugin/scala.lua
@@ -1,45 +1,44 @@
-local metals = require"metals"
-local tvp = require"metals.tvp"
-local lsp = require"lsp"
+local metals = require("metals")
+local tvp = require("metals.tvp")
+local lsp = require("lsp")
 
 vim.api.nvim_create_autocmd("LspAttach", {
-  group = vim.api.nvim_create_augroup("LspAttach_metals", {}),
-  callback = function(args)
-    if not (args.data and args.data.client_id) then
-      return
-    end
+	group = vim.api.nvim_create_augroup("LspAttach_metals", {}),
+	callback = function(args)
+		if not (args.data and args.data.client_id) then
+			return
+		end
 
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
-    if (client == nil or client.name ~= "metals") then
-      return
-    end
+		local client = vim.lsp.get_client_by_id(args.data.client_id)
+		if client == nil or client.name ~= "metals" then
+			return
+		end
 
-    local bufnr = args.buf
-    local opts = { noremap=true, silent=true, buffer=bufnr }
+		local bufnr = args.buf
+		local opts = { noremap = true, silent = true, buffer = bufnr }
 
-    -- Toggle panel with Tree Views
-    vim.keymap.set('n', 'gtv', tvp.toggle_tree_view, opts)
-    -- Reveal current current class (trait or object) in Tree View 'metalsPackages'
-    vim.keymap.set('n', 'gtf', tvp.reveal_in_tree, opts)
+		-- Toggle panel with Tree Views
+		vim.keymap.set("n", "gtv", tvp.toggle_tree_view, opts)
+		-- Reveal current current class (trait or object) in Tree View 'metalsPackages'
+		vim.keymap.set("n", "gtf", tvp.reveal_in_tree, opts)
 
-    vim.keymap.set('n', '<leader>o', metals.organize_imports, opts)
-    vim.api.nvim_buf_create_user_command(bufnr, 'OR', metals.organize_imports, { nargs = 0 })
+		vim.keymap.set("n", "<leader>o", metals.organize_imports, opts)
+		vim.api.nvim_buf_create_user_command(bufnr, "OR", metals.organize_imports, { nargs = 0 })
 
-    lsp.autoformat(client, bufnr)
-    metals.setup_dap()
-  end,
+		metals.setup_dap()
+	end,
 })
 
 local metals_config = metals.bare_config()
 metals_config.capabilities = lsp.capabilities
-metals_config.init_options.statusBarProvider = 'on'
+metals_config.init_options.statusBarProvider = "on"
 metals_config.settings = {
-  showImplicitArguments = true,
-  showInferredType = true,
-  showImplicitConversionsAndClasses = true,
-  superMethodLensesEnabled = true,
-  -- this overrides tree-sitters semantic highlighting and is not as good (yet?)
-  -- enableSemanticHighlighting = true,
+	showImplicitArguments = true,
+	showInferredType = true,
+	showImplicitConversionsAndClasses = true,
+	superMethodLensesEnabled = true,
+	-- this overrides tree-sitters semantic highlighting and is not as good (yet?)
+	-- enableSemanticHighlighting = true,
 }
 
 metals.initialize_or_attach(metals_config)
--- home/.config/nvim/plugin/lsp.lua
+++ home/.config/nvim/plugin/lsp.lua
@@ -1,174 +1,185 @@
-local config = require'lspconfig'
-local typescript = require'typescript-tools'
-local lsp = require'lsp'
-local utils = require'utils'
+local config = require("lspconfig")
+local typescript = require("typescript-tools")
+local lsp = require("lsp")
+local utils = require("utils")
 
 vim.api.nvim_create_autocmd("LspAttach", {
-  group = vim.api.nvim_create_augroup("LspAttach_general", {clear=true}),
-  callback = function(args)
-    if not (args.data and args.data.client_id) then
-      return
-    end
+	group = vim.api.nvim_create_augroup("LspAttach_general", { clear = true }),
+	callback = function(args)
+		if not (args.data and args.data.client_id) then
+			return
+		end
 
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
-    local bufnr = args.buf
+		local client = vim.lsp.get_client_by_id(args.data.client_id)
+		local bufnr = args.buf
 
-    vim.api.nvim_set_option_value('omnifunc', 'v:lua.vim.lsp.omnifunc', { buf=bufnr })
+		vim.api.nvim_set_option_value("omnifunc", "v:lua.vim.lsp.omnifunc", { buf = bufnr })
 
-    -- Mappings.
-    local opts = { silent=true, buffer=bufnr }
-    local get_opts = utils.create_get_opts(opts)
+		-- Mappings.
+		local opts = { silent = true, buffer = bufnr }
+		local get_opts = utils.create_get_opts(opts)
 
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, get_opts({ desc="lsp.declaration" }))
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, get_opts({ desc="lsp.definition" }))
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, get_opts({ desc="lsp.hover" }))
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, get_opts({ desc="lsp.implementation" }))
-    vim.keymap.set('n', 'gk', vim.lsp.buf.signature_help, get_opts({ desc="lsp.signature_help" }))
-    vim.keymap.set('i', '<C-k>', vim.lsp.buf.signature_help, get_opts({ desc="lsp.signature_help" }))
-    vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, get_opts({ desc="lsp.add_workspace_folder" }))
-    vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder,
-      get_opts({ desc="lsp.remove_workspace_folder" }))
-    vim.keymap.set('n', '<leader>wl', function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end,
-      get_opts({ desc="lsp.list_workspace_folders" }))
-    vim.keymap.set('n', 'gy', vim.lsp.buf.type_definition, get_opts({ desc="lsp.type_definition" }))
-    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, get_opts({ desc="lsp.rename" }))
-    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, get_opts({ desc="lsp.code_action" }))
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, get_opts({ desc="lsp.references" }))
-    -- buf_set_keymap('n', '<leader>ds', '<cmd>lua vim.lsp.buf.document_symbol()<CR>', opts)
-    -- buf_set_keymap('n', '<leader>ws', '<cmd>lua vim.lsp.buf.workspace_symbol()<CR>', opts)
-    -- https://www.reddit.com/r/neovim/comments/pdiflv/search_workspace_symbols/
-    vim.keymap.set('n', '<leader>ds', require('telescope.builtin').lsp_document_symbols,
-      get_opts({ desc="telescope.lsp_document_symbols" }))
-    vim.keymap.set('n', '<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols,
-      get_opts({ desc="telescope.lsp_dynamic_workspace_symbols" }))
-    -- https://github.com/nvim-telescope/telescope.nvim/issues/964
-    -- uses dynamic because most language servers return an empty list on an empty query
+		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, get_opts({ desc = "lsp.declaration" }))
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, get_opts({ desc = "lsp.definition" }))
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, get_opts({ desc = "lsp.hover" }))
+		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, get_opts({ desc = "lsp.implementation" }))
+		vim.keymap.set("n", "gk", vim.lsp.buf.signature_help, get_opts({ desc = "lsp.signature_help" }))
+		vim.keymap.set("i", "<C-k>", vim.lsp.buf.signature_help, get_opts({ desc = "lsp.signature_help" }))
+		vim.keymap.set(
+			"n",
+			"<leader>wa",
+			vim.lsp.buf.add_workspace_folder,
+			get_opts({ desc = "lsp.add_workspace_folder" })
+		)
+		vim.keymap.set(
+			"n",
+			"<leader>wr",
+			vim.lsp.buf.remove_workspace_folder,
+			get_opts({ desc = "lsp.remove_workspace_folder" })
+		)
+		vim.keymap.set("n", "<leader>wl", function()
+			print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+		end, get_opts({ desc = "lsp.list_workspace_folders" }))
+		vim.keymap.set("n", "gy", vim.lsp.buf.type_definition, get_opts({ desc = "lsp.type_definition" }))
+		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, get_opts({ desc = "lsp.rename" }))
+		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, get_opts({ desc = "lsp.code_action" }))
+		vim.keymap.set("n", "gr", vim.lsp.buf.references, get_opts({ desc = "lsp.references" }))
+		-- buf_set_keymap('n', '<leader>ds', '<cmd>lua vim.lsp.buf.document_symbol()<CR>', opts)
+		-- buf_set_keymap('n', '<leader>ws', '<cmd>lua vim.lsp.buf.workspace_symbol()<CR>', opts)
+		-- https://www.reddit.com/r/neovim/comments/pdiflv/search_workspace_symbols/
+		vim.keymap.set(
+			"n",
+			"<leader>ds",
+			require("telescope.builtin").lsp_document_symbols,
+			get_opts({ desc = "telescope.lsp_document_symbols" })
+		)
+		vim.keymap.set(
+			"n",
+			"<leader>ws",
+			require("telescope.builtin").lsp_dynamic_workspace_symbols,
+			get_opts({ desc = "telescope.lsp_dynamic_workspace_symbols" })
+		)
+		-- https://github.com/nvim-telescope/telescope.nvim/issues/964
+		-- uses dynamic because most language servers return an empty list on an empty query
 
-    vim.keymap.set('n', '<leader>i', function()
-      vim.lsp.inlay_hint(bufnr)
-    end, { silent=true, buffer=bufnr, desc="lsp.inlayhints.toggle" })
+		vim.keymap.set("n", "<leader>i", function()
+			vim.lsp.inlay_hint(bufnr)
+		end, { silent = true, buffer = bufnr, desc = "lsp.inlayhints.toggle" })
 
-    if client ~= nil and client.server_capabilities.codeLensProvider then
-      vim.api.nvim_create_augroup("lsp_codelens",  { clear=false })
-      vim.api.nvim_clear_autocmds({ buffer=bufnr, group="lsp_codelens" })
-      vim.api.nvim_create_autocmd({"BufEnter", "CursorHold", "InsertLeave"}, {
-        group = "lsp_codelens",
-        buffer = bufnr,
-        callback = vim.lsp.codelens.refresh
-      })
+		if client ~= nil and client.server_capabilities.codeLensProvider then
+			vim.api.nvim_create_augroup("lsp_codelens", { clear = false })
+			vim.api.nvim_clear_autocmds({ buffer = bufnr, group = "lsp_codelens" })
+			vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "InsertLeave" }, {
+				group = "lsp_codelens",
+				buffer = bufnr,
+				callback = vim.lsp.codelens.refresh,
+			})
 
-      vim.keymap.set("n", "<leader>cl", vim.lsp.codelens.run, get_opts({ desc="lsp.codelens.run" }))
-    end
+			vim.keymap.set("n", "<leader>cl", vim.lsp.codelens.run, get_opts({ desc = "lsp.codelens.run" }))
+		end
 
-    if client ~= nil and client.server_capabilities.inlayHintProvider then
-      vim.lsp.inlay_hint(bufnr, true)
-    end
+		if client ~= nil and client.server_capabilities.inlayHintProvider then
+			vim.lsp.inlay_hint(bufnr, true)
+		end
 
-    -- Set some keybinds conditional on server capabilities
-    if client ~= nil and lsp.formatting_supported(client) then
-      vim.keymap.set("n", "<leader>f", function() vim.lsp.buf.format{async=true} end, get_opts({ desc="lsp.format" }))
-    end
-
-    if client ~= nil and client.server_capabilities.documentHighlightProvider then
-      vim.api.nvim_create_augroup("lsp_document_highlight",  { clear=false })
-      vim.api.nvim_clear_autocmds({ buffer = bufnr, group = "lsp_document_highlight" })
-      vim.api.nvim_create_autocmd({"CursorHold", "CursorHoldI"}, {
-        group = "lsp_document_highlight",
-        buffer = bufnr,
-        callback = vim.lsp.buf.document_highlight
-      })
-      vim.api.nvim_create_autocmd("CursorMoved", {
-        group = "lsp_document_highlight",
-        buffer = bufnr,
-        callback = vim.lsp.buf.clear_references
-      })
-    end
-  end,
+		if client ~= nil and client.server_capabilities.documentHighlightProvider then
+			vim.api.nvim_create_augroup("lsp_document_highlight", { clear = false })
+			vim.api.nvim_clear_autocmds({ buffer = bufnr, group = "lsp_document_highlight" })
+			vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+				group = "lsp_document_highlight",
+				buffer = bufnr,
+				callback = vim.lsp.buf.document_highlight,
+			})
+			vim.api.nvim_create_autocmd("CursorMoved", {
+				group = "lsp_document_highlight",
+				buffer = bufnr,
+				callback = vim.lsp.buf.clear_references,
+			})
+		end
+	end,
 })
 
 -- luacheck: push ignore 122
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-  vim.lsp.diagnostic.on_publish_diagnostics, {
-    underline = true,
-    virtual_text = false,
-    signs = true,
-    update_in_insert = false,
-  }
-)
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+	underline = true,
+	virtual_text = false,
+	signs = true,
+	update_in_insert = false,
+})
 -- luacheck: pop
 
 require("mason").setup()
 require("mason-lspconfig").setup({
-  automatic_installation = true
+	automatic_installation = true,
 })
 require("neodev").setup({})
 
 local capabilities = lsp.capabilities
 
-config.bashls.setup{capabilities=capabilities}
-config.vimls.setup{capabilities=capabilities}
-config.ccls.setup{capabilities=capabilities}
-config.html.setup{capabilities=capabilities}
-config.kotlin_language_server.setup{capabilities=capabilities}
+config.bashls.setup({ capabilities = capabilities })
+config.vimls.setup({ capabilities = capabilities })
+config.ccls.setup({ capabilities = capabilities })
+config.html.setup({ capabilities = capabilities })
+config.kotlin_language_server.setup({ capabilities = capabilities })
 
-config.cssls.setup{
-  capabilities = capabilities,
-  settings = {
-    css = {
-      validate = false
-    },
-    less = {
-      validate = true
-    },
-    scss = {
-      validate = true
-    }
-  }
-}
-
-config.jsonls.setup {
-  capabilities=capabilities,
-  commands = {
-    Format = {
-      function()
-        vim.lsp.buf.range_formatting({},{0,0},{vim.fn.line("$"),0})
-      end
-    }
-  },
-}
-config.sqlls.setup{
-  capabilities=capabilities,
-  cmd={"sql-language-server", "up", "--method", "stdio"}
-}
-
-config.lua_ls.setup({
-  settings = {
-    Lua = {
-      completion = {
-        callSnippet = "Replace"
-      }
-    }
-  }
+config.cssls.setup({
+	capabilities = capabilities,
+	settings = {
+		css = {
+			validate = false,
+		},
+		less = {
+			validate = true,
+		},
+		scss = {
+			validate = true,
+		},
+	},
 })
 
-typescript.setup{
-  capabilities=capabilities,
-  settings = {
-    -- spawn additional tsserver instance to calculate diagnostics on it
-    separate_diagnostic_server = true,
-    -- "change"|"insert_leave" determine when the client asks the server about diagnostic
-    publish_diagnostic_on = "change",
-    tsserver_file_preferences = {
-      includeInlayParameterNameHints = "all",
-      includeInlayEnumMemberValueHints = true,
-      includeInlayFunctionLikeReturnTypeHints = true,
-      includeInlayFunctionParameterTypeHints = true,
-      includeInlayPropertyDeclarationTypeHints = true,
-      includeInlayVariableTypeHints = true
-    },
-    tsserver_format_options = {
-      allowIncompleteCompletions = false,
-      allowRenameOfImportPath = false
-    }
-  }
-}
+config.jsonls.setup({
+	capabilities = capabilities,
+	commands = {
+		Format = {
+			function()
+				vim.lsp.buf.range_formatting({}, { 0, 0 }, { vim.fn.line("$"), 0 })
+			end,
+		},
+	},
+})
+config.sqlls.setup({
+	capabilities = capabilities,
+	cmd = { "sql-language-server", "up", "--method", "stdio" },
+})
+
+config.lua_ls.setup({
+	settings = {
+		Lua = {
+			completion = {
+				callSnippet = "Replace",
+			},
+		},
+	},
+})
+
+typescript.setup({
+	capabilities = capabilities,
+	settings = {
+		-- spawn additional tsserver instance to calculate diagnostics on it
+		separate_diagnostic_server = true,
+		-- "change"|"insert_leave" determine when the client asks the server about diagnostic
+		publish_diagnostic_on = "change",
+		tsserver_file_preferences = {
+			includeInlayParameterNameHints = "all",
+			includeInlayEnumMemberValueHints = true,
+			includeInlayFunctionLikeReturnTypeHints = true,
+			includeInlayFunctionParameterTypeHints = true,
+			includeInlayPropertyDeclarationTypeHints = true,
+			includeInlayVariableTypeHints = true,
+		},
+		tsserver_format_options = {
+			allowIncompleteCompletions = false,
+			allowRenameOfImportPath = false,
+		},
+	},
+})

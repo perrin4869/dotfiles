839af7e9c0de3daaf0609c667fd9334f577fd592
Julian Grinblat <julian@dotcore.co.il>
2023-08-13 19:24:13 +0900

    feat(neovim): partial implementation of a dashboard utility mappings

--- home/.config/nvim/plugin/dashboard.lua
+++ home/.config/nvim/plugin/dashboard.lua
@@ -7,3 +7,17 @@ require("dashboard").setup({
 	},
 })
 
+vim.api.nvim_create_autocmd("FileType", {
+	desc = "Dashboard keymaps",
+	pattern = "dashboard",
+	group = vim.api.nvim_create_augroup("Dashboard_keymaps", { clear = true }),
+	callback = function(opts)
+		vim.keymap.set("n", "q", function()
+			vim.cmd.bdelete()
+		end, { buffer = opts.buf, silent = true, noremap = true, desc = "dashboard.exit" })
+		vim.keymap.set("n", "i", function()
+			vim.cmd.bdelete()
+			vim.cmd.startinsert()
+		end, { buffer = opts.buf, silent = true, noremap = true, desc = "dashboard.exit_and_edit" })
+	end,
+})

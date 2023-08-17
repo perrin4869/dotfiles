0ddba0a86377f0e13167c5c8078b3846546d7d1e
Julian Grinblat <julian@dotcore.co.il>
2023-08-17 13:20:12 +0900

    feat(neovim): add vim-illuminate

--- home/.config/nvim/plugin/lsp.lua
+++ home/.config/nvim/plugin/lsp.lua
@@ -81,21 +81,6 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		if client ~= nil and client.server_capabilities.inlayHintProvider then
 			vim.lsp.inlay_hint(bufnr, true)
 		end
-
-		if client ~= nil and client.server_capabilities.documentHighlightProvider then
-			vim.api.nvim_create_augroup("lsp_document_highlight", { clear = false })
-			vim.api.nvim_clear_autocmds({ buffer = bufnr, group = "lsp_document_highlight" })
-			vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-				group = "lsp_document_highlight",
-				buffer = bufnr,
-				callback = vim.lsp.buf.document_highlight,
-			})
-			vim.api.nvim_create_autocmd("CursorMoved", {
-				group = "lsp_document_highlight",
-				buffer = bufnr,
-				callback = vim.lsp.buf.clear_references,
-			})
-		end
 	end,
 })
 

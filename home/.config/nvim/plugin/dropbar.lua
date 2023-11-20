0cd9d60206dc709263771b48304dd292f75e51e5
Julian Grinblat <julian@dotcore.co.il>
2023-11-20 13:29:43 +0900

    feat(neovim): remove redundant dropbar q mapping
    https://github.com/Bekaboo/dropbar.nvim/commit/183587de8899a8a61edd974ade9c4df73e6b6a49

--- home/.config/nvim/plugin/dropbar.lua
+++ home/.config/nvim/plugin/dropbar.lua
@@ -4,13 +4,3 @@ local opts = { noremap = true, silent = true }
 local get_opts = utils.create_get_opts(opts)
 
 vim.keymap.set("n", "<space>d", require("dropbar.api").pick, get_opts({ desc = "dropbar.pick" }))
-
-vim.api.nvim_create_autocmd("FileType", {
-	group = vim.api.nvim_create_augroup("dropbar", { clear = true }),
-	pattern = "dropbar_menu",
-	callback = function()
-		vim.keymap.set("n", "q", function()
-			vim.cmd("close!")
-		end, opts)
-	end,
-})

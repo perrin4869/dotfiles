804476c43e946bb78e31c6b75c1cfde86efa4327
Julian Grinblat <julian@dotcore.co.il>
2023-11-22 01:53:38 +0900

    feat(fonts): add monaspace and configure wezterm

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -1,17 +1,56 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
-config.font = wezterm.font 'Fira Code'
-config.font_size = 16.0
-config.window_padding = {
-  left = 0,
-  right = 0,
-  top = 0,
-  bottom = 0,
+-- config.font = wezterm.font("Fira Code")
+-- https://github.com/githubnext/monaspace/issues/133
+config.font = wezterm.font({ -- Normal text
+	family = "Monaspace Neon",
+	harfbuzz_features = { "calt", "liga", "dlig", "ss01", "ss02", "ss03", "ss04", "ss05", "ss06", "ss07", "ss08" },
+	stretch = "UltraCondensed", -- This doesn't seem to do anything
+})
+
+config.font_rules = {
+	{ -- Italic
+		intensity = "Normal",
+		italic = true,
+		font = wezterm.font({
+			-- family="Monaspace Radon",  -- script style
+			family = "Monaspace Xenon", -- courier-like
+			style = "Italic",
+		}),
+	},
+
+	{ -- Bold
+		intensity = "Bold",
+		italic = false,
+		font = wezterm.font({
+			family = "Monaspace Krypton",
+			-- weight='ExtraBold',
+			weight = "Bold",
+		}),
+	},
+
+	{ -- Bold Italic
+		intensity = "Bold",
+		italic = true,
+		font = wezterm.font({
+			family = "Monaspace Xenon",
+			style = "Italic",
+			weight = "Bold",
+		}),
+	},
 }
 
-config.color_scheme = 'Gruvbox dark, pale (base16)'
+config.font_size = 15.0
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+
+config.color_scheme = "Gruvbox dark, pale (base16)"
 
 config.window_background_opacity = 0.8
 config.enable_tab_bar = false

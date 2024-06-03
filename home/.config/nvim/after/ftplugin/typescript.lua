3d8c48e55c5846ed807a4acb1a69fef63a920ce9
Julian Grinblat <julian@dotcore.co.il>
2024-06-04 02:35:47 +0900

    fix(lsp): remove references to typescript-tools

--- home/.config/nvim/after/ftplugin/typescript.lua
+++ home/.config/nvim/after/ftplugin/typescript.lua
@@ -1,6 +1,3 @@
-local c = require("typescript-tools.protocol.constants")
-local api = require("typescript-tools.api")
-
 vim.api.nvim_create_autocmd("LspAttach", {
 	group = vim.api.nvim_create_augroup("LspAttach_typescript", {}),
 	callback = function(args)
@@ -9,7 +6,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		end
 
 		local client = vim.lsp.get_client_by_id(args.data.client_id)
-		if client == nil or client.name ~= "typescript-tools" then
+		if client == nil or client.name ~= "vtsls" then
 			return
 		end
 
@@ -17,8 +14,8 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		local opts = { noremap = true, silent = true, buffer = bufnum }
 
 		vim.keymap.set("n", "<leader>o", function()
-			api.organize_imports(c.OrganizeImportsMode.All)
+			require("vtsls").commands.organize_imports(bufnum)
 		end, opts)
-		vim.api.nvim_buf_create_user_command(bufnum, "OR", "TSToolsOrganizeImports", { nargs = 0 })
+		vim.api.nvim_buf_create_user_command(bufnum, "OR", "VtsExec organize_imports", { nargs = 0 })
 	end,
 })

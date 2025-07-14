5eefbf84f86ff2bef2fca9ab70da77ce0abe5432
Julian Grinblat <julian@dotcore.co.il>
2025-07-14 13:26:25 +0900

    chore: update matchup config

--- home/.config/nvim/plugin/treesitter.lua
+++ home/.config/nvim/plugin/treesitter.lua
@@ -27,9 +27,6 @@ require("nvim-treesitter.configs").setup({
 	highlight = {
 		enable = true, -- false will disable the whole extension
 	},
-	matchup = {
-		enable = true, -- mandatory, false will disable the whole extension
-	},
 	endwise = {
 		enable = true,
 	},

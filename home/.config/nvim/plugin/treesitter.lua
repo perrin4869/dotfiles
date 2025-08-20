d3e8e7de7fb6f76b1d9d80cfce9cc9e2b69a774c
Julian Grinblat <julian@dotcore.co.il>
2025-08-21 01:30:00 +0900

    fix: remove problematic settings

--- home/.config/nvim/plugin/treesitter.lua
+++ home/.config/nvim/plugin/treesitter.lua
@@ -4,9 +4,6 @@ vim.api.nvim_create_autocmd("FileType", {
 	callback = function()
 		vim.treesitter.start()
 		vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-		vim.bo.foldmethod = "expr"
-		-- https://stackoverflow.com/questions/8316139/how-to-set-the-default-to-unfolded-when-you-open-a-file
-		vim.bo.foldlevelstart = 99
 		vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
 	end,
 })

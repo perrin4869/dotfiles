39249915bfda12fd0cd7a62178b4a8912031bdb0
Julian Grinblat <julian@dotcore.co.il>
2025-09-16 22:45:35 +0900

    fix(neovim): enable tree-sitter indents only on available languages

--- home/.config/nvim/plugin/treesitter.lua
+++ home/.config/nvim/plugin/treesitter.lua
@@ -1,9 +1,25 @@
+-- cache for per-language indent availability
+local indent_cache = {}
+
+local function has_indent(ft)
+	local lang = vim.treesitter.language.get_lang(ft)
+	if lang ~= nil and indent_cache[lang] == nil then
+		indent_cache[lang] = vim.treesitter.query.get(lang, "indents") ~= nil
+	end
+	return indent_cache[lang]
+end
+
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = require("nvim-treesitter").get_installed(),
-	callback = function()
+	callback = function(args)
 		vim.treesitter.start()
 		vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-		vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+
+		-- scala for example doesn't have indents yet
+		-- https://github.com/nvim-treesitter/nvim-treesitter/tree/42fc28ba918343ebfd5565147a42a26580579482/queries/scala
+		if has_indent(vim.bo[args.buf].ft) then
+			vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+		end
 
 		-- https://stackoverflow.com/questions/8316139/how-to-set-the-default-to-unfolded-when-you-open-a-file
 		vim.opt_local.foldlevelstart = 99

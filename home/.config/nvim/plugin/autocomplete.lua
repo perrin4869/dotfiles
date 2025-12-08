cd3881c842fb2048f3014f5751a0056e587898ea
Julian Grinblat <julian@dotcore.co.il>
2025-12-08 17:28:56 +0900

    feat: auto complete mappings disable C-n C-p

--- home/.config/nvim/plugin/autocomplete.lua
+++ home/.config/nvim/plugin/autocomplete.lua
@@ -1,6 +1,24 @@
 local cmp = require("cmp")
 local lspkind = require("lspkind")
 
+local function nav_item(f)
+	return function()
+		if cmp.visible() then
+			f()
+		else
+			cmp.complete()
+		end
+	end
+end
+
+local prev_item = nav_item(function()
+	cmp.select_prev_item({ behavior = cmp.SelectBehavior.Insert })
+end)
+
+local next_item = nav_item(function()
+	cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
+end)
+
 cmp.setup({
 	snippet = {
 		expand = function(args)
@@ -15,8 +33,11 @@ cmp.setup({
 		-- help complete_CTRL-Y
 		["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 
-		["<C-k>"] = cmp.mapping.select_prev_item(),
-		["<C-j>"] = cmp.mapping.select_next_item(),
+		["<C-n>"] = cmp.config.disable,
+		["<C-p>"] = cmp.mapping.disable,
+
+		["<C-k>"] = cmp.mapping(prev_item, { "i", "c" }),
+		["<C-j>"] = cmp.mapping(next_item, { "i", "c" }),
 	}),
 	formatting = {
 		format = lspkind.cmp_format({ mode = "symbol_text" }),
@@ -54,20 +75,26 @@ cmp.setup.filetype("gitcommit", {
 local cmdline_mappings = cmp.mapping.preset.cmdline({
 	-- to preserve C-n and C-p behavior, only apply the mappings if an item is already selected
 	-- you can select an item if none has been selected yet by using `Tab`
-	["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }), { "i", "c" }),
-	["<C-j>"] = cmp.mapping(cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }), { "i", "c" }),
-	["<C-n>"] = cmp.mapping(function(fallback)
-		if cmp.visible() and cmp.get_selected_entry() then
-			return cmp.select_next_item()
-		end
-		fallback()
-	end, { "i", "c" }),
-	["<C-p>"] = cmp.mapping(function(fallback)
-		if cmp.visible() and cmp.get_selected_entry() then
-			return cmp.select_prev_item()
-		end
-		fallback()
-	end, { "i", "c" }),
+	["<C-k>"] = cmp.mapping(prev_item, { "i", "c" }),
+	["<C-j>"] = cmp.mapping(next_item, { "i", "c" }),
+	["<C-n>"] = cmp.config.disable,
+	["<C-p>"] = cmp.config.disable,
+
+	-- the idea behind the mappings below was ta allow navigating commands up and down
+	-- while also allowing navigating completions up and down when in focus
+	--
+	-- ["<C-n>"] = cmp.mapping(function(fallback)
+	-- 	if cmp.visible() and cmp.get_selected_entry() then
+	-- 		return cmp.select_next_item()
+	-- 	end
+	-- 	fallback()
+	-- end, { "i", "c" }),
+	-- ["<C-p>"] = cmp.mapping(function(fallback)
+	-- 	if cmp.visible() and cmp.get_selected_entry() then
+	-- 		return cmp.select_prev_item()
+	-- 	end
+	-- 	fallback()
+	-- end, { "i", "c" }),
 	["<CR>"] = cmp.mapping(function(fallback)
 		if cmp.visible() and cmp.get_selected_entry() then
 			return cmp.confirm()

0d99de825ff20977e9abf76c222fab35ce630376
Julian Grinblat <julian@dotcore.co.il>
2025-12-09 13:30:01 +0900

    feat(neovim): dropbar mapping

--- home/.config/nvim/plugin/dropbar.lua
+++ home/.config/nvim/plugin/dropbar.lua
@@ -3,4 +3,4 @@ local utils = require("utils")
 local opts = { noremap = true, silent = true }
 local get_opts = utils.create_get_opts(opts)
 
-vim.keymap.set("n", "<leader>D", require("dropbar.api").pick, get_opts({ desc = "dropbar.pick" }))
+vim.keymap.set("n", "<C-g>b", require("dropbar.api").pick, get_opts({ desc = "dropbar.pick" }))

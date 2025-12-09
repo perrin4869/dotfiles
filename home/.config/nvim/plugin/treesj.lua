9e22e535fad5ab27ecaa860602c4d257503e8ebc
Julian Grinblat <julian@dotcore.co.il>
2025-12-09 14:51:32 +0900

    feat(neovim): improve treesj mappings installation

--- home/.config/nvim/plugin/treesj.lua
+++ home/.config/nvim/plugin/treesj.lua
@@ -2,7 +2,12 @@ require("treesj").setup({
 	use_default_keymaps = false,
 })
 
--- <Leader>s is used for flash.nvim
-vim.keymap.set("n", "<leader>mm", require("treesj").toggle)
-vim.keymap.set("n", "<leader>mj", require("treesj").join)
-vim.keymap.set("n", "<leader>ms", require("treesj").split)
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = require("nvim-treesitter").get_installed(),
+	callback = function(args)
+		-- <Leader>s is used for flash.nvim
+		vim.keymap.set("n", "<leader>mm", require("treesj").toggle, { buffer = args.buf, desc = "treesj.toggle" })
+		vim.keymap.set("n", "<leader>mj", require("treesj").join, { buffer = args.buf, desc = "treesj.join" })
+		vim.keymap.set("n", "<leader>ms", require("treesj").split, { buffer = args.buf, desc = "treesj.split" })
+	end,
+})

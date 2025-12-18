fb38cbe7f188c728708e69b1916e9f4e40e07505
Julian Grinblat <julian@dotcore.co.il>
2025-12-19 05:23:17 +0900

    chore(neovim): use built in nvim-next integration

--- home/.config/nvim/plugin/unimpaired.lua
+++ home/.config/nvim/plugin/unimpaired.lua
@@ -10,18 +10,5 @@ end)
 vim.keymap.set("n", "[b", prev_buffers, { silent = true, desc = "Go to previous buffer" })
 vim.keymap.set("n", "]b", next_buffers, { silent = true, desc = "Go to next buffer" })
 
-local prev_qf_item, next_qf_item = next_move.make_repeatable_pair(function(_)
-	local status, err = pcall(vim.cmd, "cprevious")
-	if not status then
-		vim.notify("No more items", vim.log.levels.INFO)
-	end
-end, function(_)
-	local status, err = pcall(vim.cmd, "cnext")
-	if not status then
-		vim.notify("No more items", vim.log.levels.INFO)
-	end
-end)
-
 -- see help [q and help ]q for the default mappings in neovim >= 0.11
-vim.keymap.set("n", "[q", prev_qf_item, { desc = "nvim-next: prev qfix" })
-vim.keymap.set("n", "]q", next_qf_item, { desc = "nvim-next: next qfix" })
+require("nvim-next.integrations").quickfix().setup()

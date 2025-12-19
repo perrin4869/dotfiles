8101531a15f5cb4ffba3b111b314de2aaf087523
Julian Grinblat <julian@dotcore.co.il>
2025-12-20 04:32:40 +0900

    feat(neovim): add defer utility

--- home/.config/nvim/lua/defer.lua
+++ home/.config/nvim/lua/defer.lua
@@ -1,5 +1,52 @@
 local M = {}
 
+-- Stores setup functions and package names
+local initializers = {}
+-- Cache for loaded modules
+local cache = {}
+
+--- Defines the initialization logic for a plugin
+--- @param name string The module name to require
+--- @param setup_fn function The function to run once the module is required
+--- @param package string? Optional: The folder name in 'opt/' to packadd
+function M.on_load(name, setup_fn, package)
+	initializers[name] = {
+		setup = setup_fn,
+		package = package,
+	}
+end
+
+--- Internal: Bootstraps the plugin on demand
+local function ensure(name)
+	if not cache[name] then
+		local config = initializers[name]
+
+		-- Load from opt/ if a package name was provided
+		if config and config.package then
+			vim.cmd("packadd " .. config.package)
+		end
+
+		local module = require(name)
+
+		-- Run setup if it exists
+		if config and config.setup then
+			config.setup(module)
+		end
+
+		cache[name] = module
+	end
+	return cache[name]
+end
+
+--- Curried wrapper for lazy execution
+function M.with(name)
+	return function(callback)
+		return function(...)
+			return callback(ensure(name), ...)
+		end
+	end
+end
+
 function M.lazy(fn)
 	local value
 	local ran = false
--- home/.config/nvim/plugin/flash.lua
+++ home/.config/nvim/plugin/flash.lua
@@ -1,37 +1,51 @@
-local flash = require("flash")
+local defer = require("defer")
 local utils = require("utils")
 
-flash.setup({
-	modes = {
-		char = {
-			-- disables f/F/t/T, would need to integrate with nvim-next
-			enabled = false,
-		},
-	},
-})
+defer.on_load("flash", function(flash)
+	flash.setup({
+		modes = { char = { enabled = false } },
+	})
+end) -- , "flash.nvim") -- "flash.nvim" is the folder name in pack/*/opt/
+local with_flash = defer.with("flash")
+
+local jump = with_flash(function(f)
+	f.jump()
+end)
+local remote = with_flash(function(f)
+	f.remote()
+end)
+local toggle = with_flash(function(f)
+	f.toggle()
+end)
+local treesitter = with_flash(function(f)
+	f.treesitter()
+end)
+local treesitter_search = with_flash(function(f)
+	f.treesitter_search()
+end)
 
 local opts = { silent = true }
 local get_opts = utils.create_get_opts(opts)
 
-vim.keymap.set({ "n", "x", "o" }, "<leader>s", flash.jump, get_opts({ desc = "flash.jump" }))
-vim.keymap.set("o", "r", flash.remote, get_opts({ desc = "flash.remote" }))
-vim.keymap.set("c", "<C-s>", flash.toggle, get_opts({ desc = "flash.toggle" }))
+vim.keymap.set({ "n", "x", "o" }, "<leader>s", jump, get_opts({ desc = "flash.jump" }))
+vim.keymap.set("o", "r", remote, get_opts({ desc = "flash.remote" }))
+vim.keymap.set("c", "<C-s>", toggle, get_opts({ desc = "flash.toggle" }))
 
+-- Treesitter logic
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = require("nvim-treesitter").get_installed(),
 	callback = function(args)
-		-- duplicate functionality with tree-hopper
 		vim.keymap.set(
 			{ "n", "x", "o" },
 			"<leader>S",
-			flash.treesitter,
+			treesitter,
 			get_opts({ buffer = args.buf, desc = "flash.treesitter" })
 		)
 
 		vim.keymap.set(
 			{ "o", "x" },
 			"R",
-			flash.treesitter_search,
+			treesitter_search,
 			get_opts({ buffer = args.buf, desc = "flash.treesitter_search" })
 		)
 	end,

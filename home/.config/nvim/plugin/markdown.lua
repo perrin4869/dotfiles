1f553589a59c7afb2e027a5afdcce3760410b013
Julian Grinblat <julian@dotcore.co.il>
2025-12-20 20:42:00 +0900

    chore(neovim): lazy load render-markdown

--- home/.config/nvim/plugin/markdown.lua
+++ home/.config/nvim/plugin/markdown.lua
@@ -1,3 +1,12 @@
 vim.g.render_markdown_config = {
 	completions = { lsp = { enabled = true } },
 }
+
+vim.api.nvim_create_autocmd("FileType", {
+	once = true,
+	group = vim.api.nvim_create_augroup("Initialize_render_markdown", {}),
+	pattern = "markdown",
+	callback = function()
+		vim.cmd.packadd("render-markdown.nvim")
+	end,
+})

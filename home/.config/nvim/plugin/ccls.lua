74a92b3e57c68ef7c9ee374210b007902744ee92
Julian Grinblat <julian@dotcore.co.il>
2025-12-20 21:04:50 +0900

    chore(neovim): lazy load ccls

--- home/.config/nvim/plugin/ccls.lua
+++ home/.config/nvim/plugin/ccls.lua
@@ -2,6 +2,7 @@ vim.api.nvim_create_autocmd("FileType", {
 	once = true,
 	pattern = vim.lsp.config.ccls.filetypes,
 	callback = function()
+		vim.cmd.packadd("ccls.nvim")
 		require("ccls").setup()
 
 		vim.lsp.enable("ccls")

c555360c2d35ca0b03adf609f9a1ca92afb64664
Julian Grinblat <julian@dotcore.co.il>
2025-12-25 03:29:54 +0900

    lazy diagnostic and next

--- home/.config/nvim/plugin/unimpaired.lua
+++ home/.config/nvim/plugin/unimpaired.lua
@@ -1,14 +1,17 @@
-local next_move = require("nvim-next.move")
+local defer = require("defer")
+defer.on_event(function()
+	local next_move = require("nvim-next.move")
 
--- buffers
-local prev_buffers, next_buffers = next_move.make_repeatable_pair(function()
-	vim.cmd("bprevious")
-end, function()
-	vim.cmd("bnext")
-end)
+	-- buffers
+	local prev_buffers, next_buffers = next_move.make_repeatable_pair(function()
+		vim.cmd("bprevious")
+	end, function()
+		vim.cmd("bnext")
+	end)
 
-vim.keymap.set("n", "[b", prev_buffers, { silent = true, desc = "Go to previous buffer" })
-vim.keymap.set("n", "]b", next_buffers, { silent = true, desc = "Go to next buffer" })
+	vim.keymap.set("n", "[b", prev_buffers, { silent = true, desc = "Go to previous buffer" })
+	vim.keymap.set("n", "]b", next_buffers, { silent = true, desc = "Go to next buffer" })
 
--- see help [q and help ]q for the default mappings in neovim >= 0.11
-require("nvim-next.integrations").quickfix().setup()
+	-- see help [q and help ]q for the default mappings in neovim >= 0.11
+	require("nvim-next.integrations").quickfix().setup()
+end, "BufEnter", { name = "unimpaired" })

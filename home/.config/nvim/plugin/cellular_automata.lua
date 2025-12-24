bf8277b531227ec0d409e239befa19654a2bdd1b
Julian Grinblat <julian@dotcore.co.il>
2025-12-24 17:39:14 +0900

    feat: lazy automata

--- home/.config/nvim/plugin/cellular_automata.lua
+++ home/.config/nvim/plugin/cellular_automata.lua
@@ -1,8 +1,15 @@
-local automaton = require("cellular-automaton")
+local defer = require("defer")
+
+defer.on_load("cellular-automaton", function() end, "cellular-automaton.nvim")
+defer.hook("cellular-automaton")
+defer.cmd("CellularAutomaton", "cellular-automaton")
 
 -- for example "make_it_rain"
-local animations = vim.tbl_keys(automaton.animations)
+local get_animations = defer.lazy(function()
+	return vim.tbl_keys(require("cellular-automaton").animations)
+end)
 local fml = function()
-	automaton.start_animation(animations[math.random(1, #animations)])
+	local animations = get_animations()
+	require("cellular-automaton").start_animation(animations[math.random(1, #animations)])
 end
 vim.keymap.set("n", "<leader>fml", fml, { noremap = true, silent = true })

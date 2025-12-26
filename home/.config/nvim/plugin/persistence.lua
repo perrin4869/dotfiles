98d67dfafed2e16fd737dd623e5271bec7393443
Julian Grinblat <julian@dotcore.co.il>
2025-12-27 05:08:27 +0900

    feat(neovim): lazy persistence.nvim

--- home/.config/nvim/plugin/persistence.lua
+++ home/.config/nvim/plugin/persistence.lua
@@ -1,18 +1,26 @@
-local persistence = require("persistence")
+local defer = require("defer")
 local utils = require("utils")
 
-persistence.setup()
+defer.on_load("persistence", function(persistence)
+	persistence.setup()
+end, "persistence.nvim")
+defer.hook("persistence")
+defer.on_event("persistence", "BufReadPre")
 
 local opts = { noremap = true, silent = true }
 local get_opts = utils.create_get_opts(opts)
 
 -- restore the session for the current directory
-vim.keymap.set("n", "<leader>qs", persistence.load, get_opts({ desc = "persistence.load" }))
+vim.keymap.set("n", "<leader>qs", function()
+	require("persistence").load()
+end, get_opts({ desc = "persistence.load" }))
 
 -- restore the last session
 vim.keymap.set("n", "<leader>ql", function()
-	persistence.load({ last = true })
+	require("persistence").load({ last = true })
 end, get_opts({ desc = "persistence.load_last" }))
 
 -- stop Persistence => session won't be saved on exit
-vim.keymap.set("n", "<leader>qd", persistence.stop, get_opts({ desc = "persistence.stop" }))
+vim.keymap.set("n", "<leader>qd", function()
+	require("persistence").stop()
+end, get_opts({ desc = "persistence.stop" }))
--- home/.config/nvim/plugin/workspaces.lua
+++ home/.config/nvim/plugin/workspaces.lua
@@ -1,11 +1,11 @@
 local pickers = require("pickers")
-local persistence = require("persistence")
 
 require("workspaces").setup({
 	auto_open = true,
 	hooks = {
 		-- first argument is the workspace name
 		open_pre = function(_, path)
+			local persistence = require("persistence")
 			if persistence.active() then
 				local cwd = vim.uv.cwd()
 				if cwd and vim.fs.normalize(cwd) ~= vim.fs.normalize(path) then
@@ -15,6 +15,7 @@ require("workspaces").setup({
 			end
 		end,
 		open = function()
+			local persistence = require("persistence")
 			persistence.start()
 			if vim.uv.fs_stat(require("persistence").current()) ~= nil then
 				persistence.load()

e4dfe5a99c9d9f4bcfbd5d01559ed89a762afd11
Julian Grinblat <julian@dotcore.co.il>
2025-12-29 04:59:56 +0900

    perf(neovim): lazy dap

--- home/.config/nvim/plugin/dap.lua
+++ home/.config/nvim/plugin/dap.lua
@@ -1,8 +1,38 @@
 local defer = require("defer")
+local utils = require("utils")
+local opts = { noremap = true, silent = true }
+local get_opts = utils.create_get_opts(opts)
 
-defer.on_event(function()
+local function focus_repl()
+	for _, win in ipairs(vim.api.nvim_list_wins()) do
+		local buf = vim.api.nvim_win_get_buf(win)
+		if vim.bo[buf].filetype == "dap-repl" then
+			vim.api.nvim_set_current_win(win)
+			return true
+		end
+	end
+	return false
+end
+
+defer.pack("nvim-dap-virtual-text", "nvim-dap-virtual-text")
+defer.on_load("nvim-dap-virtual-text", function()
+	require("nvim-dap-virtual-text").setup()
+end)
+
+defer.pack("dap-ui", "nvim-dap-ui")
+defer.on_load("dapui", function()
+	-- nvim-dap-ui
+	require("dapui").setup()
+end)
+defer.hook("dapui")
+
+local with_dap = defer.with("dap")
+local with_dapui = defer.with("dapui")
+local call = defer.call
+
+defer.pack("dap", "nvim-dap")
+defer.on_load("dap", function()
 	local dap = require("dap")
-	local utils = require("utils")
 
 	dap.adapters["pwa-node"] = {
 		type = "server",
@@ -95,57 +125,6 @@ defer.on_event(function()
 	vim.fn.sign_define("DapBreakpointRejected", { text = "🟦", texthl = "", linehl = "", numhl = "" })
 	vim.fn.sign_define("DapStopped", { text = "⭐️", texthl = "", linehl = "", numhl = "" })
 
-	-- Mappings.
-	local prefix = "<leader>d"
-	local opts = { noremap = true, silent = true }
-	local get_opts = utils.create_get_opts(opts)
-
-	local function focus_repl()
-		for _, win in ipairs(vim.api.nvim_list_wins()) do
-			local buf = vim.api.nvim_win_get_buf(win)
-			if vim.bo[buf].filetype == "dap-repl" then
-				vim.api.nvim_set_current_win(win)
-				return true
-			end
-		end
-		return false
-	end
-
-	vim.keymap.set("n", prefix .. "t", dap.toggle_breakpoint, get_opts({ desc = "dap.toggle_breakpoint" }))
-	vim.keymap.set("n", prefix .. "H", function()
-		dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
-	end, get_opts({ desc = "dap.set_breakpoint" }))
-	vim.keymap.set("n", "<c-k>", dap.step_out, get_opts({ desc = "dap.step_out" }))
-	vim.keymap.set("n", "<c-l>", dap.step_into, get_opts({ desc = "dap.step_into" }))
-	vim.keymap.set("n", "<c-j>", dap.step_over, get_opts({ desc = "dap.step_over" }))
-	vim.keymap.set("n", "<c-h>", dap.continue, get_opts({ desc = "dap.continue" }))
-	vim.keymap.set("n", prefix .. "k", dap.up, get_opts({ desc = "dap.up" }))
-	vim.keymap.set("n", prefix .. "j", dap.down, get_opts({ desc = "dap.down" }))
-	vim.keymap.set("n", prefix .. "d", function()
-		dap.disconnect({ terminateDebuggee = true })
-		dap.close()
-	end, get_opts({ desc = "dap.disconnect" }))
-	vim.keymap.set("n", prefix .. "r", function()
-		if focus_repl() == false then
-			local _, win = dap.repl.open({}, "vsplit")
-			vim.api.nvim_set_current_win(win)
-			vim.cmd('exe Resize("vertical", "", "35%")')
-		end
-	end, get_opts({ desc = "dap.repl.open_focus" }))
-	vim.keymap.set("n", prefix .. "R", function()
-		dap.repl.toggle({}, "vsplit")
-		vim.cmd('wincmd h | exe Resize("vertical", "", "35%")')
-	end, get_opts({ desc = "dap.repl.toggle" }))
-	vim.keymap.set("n", prefix .. "e", function()
-		dap.set_exception_breakpoints({ "all" })
-	end, get_opts({ desc = "dap.set_exception_breakpoints" }))
-	vim.keymap.set("n", prefix .. "i", function()
-		require("dap.ui.widgets").hover(require("dap.utils").get_visual_selection_text())
-	end, get_opts({ desc = "dap.ui.widgets.hover" }))
-	vim.keymap.set("n", prefix .. "?", function()
-		require("dap.ui.widgets").cursor_float(require("dap.ui.widgets").scopes)
-	end, get_opts({ desc = "dap.ui.widgets.scopes" }))
-
 	-- Map K to hover while session is active.
 	local keymap_restore = {}
 	dap.listeners.after["event_initialized"]["me"] = function()
@@ -177,28 +156,14 @@ defer.on_event(function()
 		callback = function()
 			vim.keymap.set("n", "q", function()
 				vim.cmd("close!")
-			end, opts)
+			end, get_opts({ desc = "dap.close_float" }))
 		end,
 	})
 
-	require("nvim-dap-virtual-text").setup()
+	defer.ensure("nvim-dap-virtual-text")
 
-	defer.on_load("dapui", function()
-		-- nvim-dap-ui
-		require("dapui").setup()
-	end)
-	defer.hook("dapui")
-
-	vim.keymap.set("n", prefix .. "u", function()
-		require("dapui").toggle()
-	end, get_opts({ desc = "dapui.toggle" }))
-
-	dap.listeners.before.attach.dapui_config = function()
-		require("dapui").open()
-	end
-	dap.listeners.before.launch.dapui_config = function()
-		require("dapui").open()
-	end
+	dap.listeners.before.attach.dapui_config = with_dapui(call("open"))
+	dap.listeners.before.launch.dapui_config = with_dapui(call("open"))
 	dap.listeners.before.event_terminated["dapui_config"] = function()
 		focus_repl()
 
@@ -208,19 +173,88 @@ defer.on_event(function()
 	-- dap.listeners.before.event_exited["dapui_config"] = function()
 	-- 	require("dapui").close()
 	-- end
+end)
+defer.very_lazy("dap")
 
-	-- telescope-dap
-	local pickers = require("pickers")
-	pickers.map(prefix .. "c", function(telescope)
-		telescope.commands()
-	end, { prefix = false, desc = "dap.commands" })
-	pickers.map(prefix .. "b", function(telescope)
-		telescope.list_breakpoints()
-	end, { prefix = false, desc = "dap.list_breakpoints" })
-	pickers.map(prefix .. "v", function(telescope)
-		telescope.variables()
-	end, { prefix = false, desc = "dap.variables" })
-	pickers.map(prefix .. "f", function(telescope)
-		telescope.frames()
-	end, { prefix = false, desc = "dap.frames" })
-end, "BufReadPost", { name = "dap" })
+-- Mappings.
+local prefix = "<leader>d"
+local map = function(mode, lhs, rhs, desc)
+	local t = type(rhs)
+	if t == "function" then
+		vim.keymap.set(mode, lhs, rhs, get_opts({ desc = "dap." .. desc }))
+	elseif t == "string" then
+		vim.keymap.set(mode, lhs, with_dap(call(rhs)), get_opts({ desc = "dap." .. rhs }))
+	else -- table
+		vim.keymap.set(mode, lhs, with_dap(call(table.unpack(rhs))), get_opts({ desc = "dap." .. rhs[1] }))
+	end
+end
+
+local set_breakpoint = with_dap(function(dap)
+	dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
+end)
+
+local disconnect = with_dap(function(dap)
+	dap.disconnect({ terminateDebuggee = true })
+	dap.close()
+end)
+
+local repl_open_focus = with_dap(function(dap)
+	if focus_repl() == false then
+		local _, win = dap.repl.open({}, "vsplit")
+		vim.api.nvim_set_current_win(win)
+		vim.cmd('exe Resize("vertical", "", "35%")')
+	end
+end)
+
+local repl_toggle = with_dap(function(dap)
+	dap.repl.toggle({}, "vsplit")
+	vim.cmd('wincmd h | exe Resize("vertical", "", "35%")')
+end)
+
+local ui_widgets_hover = with_dap(function()
+	require("dap.ui.widgets").hover(require("dap.utils").get_visual_selection_text())
+end)
+
+local ui_widgets_scopes = with_dap(function()
+	require("dap.ui.widgets").cursor_float(require("dap.ui.widgets").scopes)
+end)
+
+map("n", prefix .. "t", "toggle_breakpoint")
+map("n", prefix .. "H", set_breakpoint, "set_breakpoint")
+map("n", "<c-k>", "step_out")
+map("n", "<c-l>", "step_into")
+map("n", "<c-j>", "step_over")
+map("n", "<c-h>", "continue")
+map("n", prefix .. "k", "up")
+map("n", prefix .. "j", "down")
+map("n", prefix .. "d", disconnect, "disconnect")
+map("n", prefix .. "r", repl_open_focus, "repl.open_focus")
+map("n", prefix .. "R", repl_toggle, "repl.toggle")
+map("n", prefix .. "e", { "set_exception_breakpoints", { "all" } })
+map("n", prefix .. "i", ui_widgets_hover, "ui.widgets.hover")
+map("n", prefix .. "?", ui_widgets_scopes, "ui.widgets.scopes")
+
+vim.keymap.set("n", prefix .. "u", with_dapui(call("toggle")), get_opts({ desc = "dapui.toggle" }))
+
+-- telescope-dap
+local pickers = require("pickers")
+pickers.map(prefix .. "c", function(telescope)
+	with_dap(function()
+		telescope.extensions.dap.commands()
+	end)()
+end, { prefix = false, desc = "dap.commands" })
+pickers.map(prefix .. "b", function(telescope)
+	with_dap(function()
+		telescope.extensions.dap.list_breakpoints()
+	end)()
+end, { prefix = false, desc = "dap.list_breakpoints" })
+pickers.map(prefix .. "v", function(telescope)
+	with_dap(function()
+		telescope.extensions.dap.variables()
+	end)()
+end, { prefix = false, desc = "dap.variables" })
+pickers.map(prefix .. "f", function(telescope)
+	with_dap(function()
+		telescope.extensions.dap.frames()
+	end)()
+end, { prefix = false, desc = "dap.frames" })

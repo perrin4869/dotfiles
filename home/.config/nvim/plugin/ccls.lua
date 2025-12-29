60caa1ba4b47303cdefb1d4939e6c90865863ed5
Julian Grinblat <julian@dotcore.co.il>
2025-12-30 05:50:34 +0900

    fix

--- home/.config/nvim/plugin/ccls.lua
+++ home/.config/nvim/plugin/ccls.lua
@@ -1,5 +1,5 @@
 local defer = require("defer")
-defer.on_event(function()
+defer.on_load("ccls", function()
 	vim.api.nvim_create_autocmd("FileType", {
 		once = true,
 		pattern = vim.lsp.config.ccls.filetypes,
@@ -10,4 +10,5 @@ defer.on_event(function()
 			vim.lsp.enable("ccls")
 		end,
 	})
-end, { "BufReadPre", "BufNewFile" }, { name = "ccls" })
+end)
+defer.on_event("ccls", { "BufReadPre", "BufNewFile" })

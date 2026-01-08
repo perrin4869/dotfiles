c432ab680b1088d5281319cc873c89196e8a8148
Julian Grinblat <julian@dotcore.co.il>
2026-01-08 15:59:08 +0900

    feat(neovim): replace vim-floaterm with nvzone/floaterm

--- home/.config/nvim/plugin/floaterm.lua
+++ home/.config/nvim/plugin/floaterm.lua
@@ -1,7 +1,50 @@
 local defer = require("defer")
-defer.pack("floaterm", "vim-floaterm")
-defer.cmd("FloatermToggle", "floaterm")
-defer.cmd("FloatermNew", "floaterm")
-defer.cmd("FloatermPrev", "floaterm")
-defer.cmd("FloatermNext", "floaterm")
-defer.cmd("FloatermKill", "floaterm")
+defer.pack("floaterm")
+defer.very_lazy("floaterm")
+defer.on_load("floaterm", function()
+	require("floaterm").setup({
+		border = true,
+		-- https://github.com/nvzone/floaterm/issues/35
+		mappings = {
+			term = function(buf)
+				vim.keymap.set("n", "<ESC>", function()
+					require("floaterm").toggle()
+				end, { buffer = buf })
+			end,
+		},
+	})
+end)
+
+local call = defer.call
+local with_floaterm = defer.with("floaterm")
+local with_floaterm_api = function(cb)
+	return defer.with("floaterm")(function()
+		cb(require("floaterm.api"))
+	end)
+end
+
+local function map(lhs, rhs, desc)
+	vim.keymap.set({ "n", "t" }, lhs, rhs, { desc = "floaterm." .. desc, silent = true, noremap = true })
+end
+
+map("<M-t>", with_floaterm(call("toggle")), "toggle")
+
+map(
+	"<F6>",
+	with_floaterm(function(floaterm)
+		if not require("floaterm.state").terminals then
+			floaterm.toggle()
+			return
+		end
+		if vim.bo.ft ~= "Floaterm" then
+			floaterm.toggle()
+		end
+		require("floaterm.api").new_term()
+	end),
+	"new_term"
+)
+map("<F7>", with_floaterm_api(call("cycle_term_bufs", "prev")), "cycle_prev")
+map("<F8>", with_floaterm_api(call("cycle_term_bufs")), "cycle_next")
+map("<F12>", with_floaterm(call("toggle")), "toggle")
+
+map("<F9>", with_floaterm_api(call("delete_term")), "delete_term")

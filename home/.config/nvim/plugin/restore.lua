417e2f6eeee96ad3f004a5749648d57000244382
Julian Grinblat <julian@dotcore.co.il>
2026-01-15 03:42:19 +0900

    fix(neovim): restore quitpre window detection

--- home/.config/nvim/plugin/restore.lua
+++ home/.config/nvim/plugin/restore.lua
@@ -1,6 +1,7 @@
 vim.api.nvim_create_autocmd("QuitPre", {
 	callback = function()
-		local tree_wins = {}
+		local restore_wins = {}
+		local ignore_wins = {}
 		local floating_wins = {}
 		-- consider also trying vim.fn.winlayout()
 		local wins = vim.api.nvim_list_wins()
@@ -8,14 +9,15 @@ vim.api.nvim_create_autocmd("QuitPre", {
 			local bufnr = vim.api.nvim_win_get_buf(w)
 			local ft = vim.bo[bufnr].filetype
 			-- ignore empty buffers (no name and no ft)
-			if require("restore").ft(ft) or (ft == "" and vim.api.nvim_buf_get_name(bufnr) == "") then
-				table.insert(tree_wins, w)
-			end
-			if vim.api.nvim_win_get_config(w).relative ~= "" then
+			if require("restore").ft(ft) then
+				table.insert(restore_wins, w)
+			elseif ft == "" and vim.api.nvim_buf_get_name(bufnr) == "" then
+				table.insert(ignore_wins, w)
+			elseif vim.api.nvim_win_get_config(w).relative ~= "" then
 				table.insert(floating_wins, w)
 			end
 		end
-		if 1 == #wins - #floating_wins - #tree_wins then
+		if 1 == #wins - #floating_wins - #restore_wins - #ignore_wins then
 			-- save the session before closing the windows, otherwise the tree state does not get saved
 			local persistence = require("persistence")
 			if persistence.active() then
@@ -24,7 +26,7 @@ vim.api.nvim_create_autocmd("QuitPre", {
 			end
 
 			-- Should quit, so we close all invalid windows.
-			for _, w in ipairs(tree_wins) do
+			for _, w in ipairs(restore_wins) do
 				vim.api.nvim_win_close(w, true)
 			end
 		end

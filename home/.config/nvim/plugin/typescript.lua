75a8dab29e4dbbd7e0b2e2b51153c2be6f455365
Julian Grinblat <julian@dotcore.co.il>
2026-02-18 18:07:28 +0900

    feat(neovim): add organize_imports for tsgo

--- home/.config/nvim/plugin/typescript.lua
+++ home/.config/nvim/plugin/typescript.lua
@@ -6,11 +6,38 @@ vim.api.nvim_create_autocmd('LspAttach', {
 		end
 
 		local client = vim.lsp.get_client_by_id(args.data.client_id)
-		if client ~= nil and client.name == 'vtsls' then
-			require('lsp').organize_imports(function()
-				require('vtsls').commands.organize_imports(args.buf)
-			end, args.buf)
+		if client ~= nil then
+			if client.name == 'vtsls' then
+				require('lsp').organize_imports(function()
+					require('vtsls').commands.organize_imports(args.buf)
+				end, args.buf)
+			elseif client.name == 'tsgo' then
+				require('lsp').organize_imports(function()
+					local params = vim.lsp.util.make_range_params(0, client.offset_encoding)
+					params.context = {
+						only = { 'source.organizeImports' },
+						diagnostics = {},
+					}
+
+					vim.lsp.buf_request(args.buf, 'textDocument/codeAction', params, function(err, result)
+						if err or not result or vim.tbl_isempty(result) then
+							return
+						end
+
+						local action = result[1]
+
+						if action.edit then
+							vim.lsp.util.apply_workspace_edit(action.edit, client.offset_encoding)
+						end
+
+						if action.command then
+							client:exec_cmd(action.command)
+						end
+					end)
+				end, args.buf)
+			end
 		end
+
 		-- TODO: add organize imports for tsgo
 		-- https://github.com/microsoft/typescript-go/issues/1615
 	end,

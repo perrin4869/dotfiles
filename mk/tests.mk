ae36842fda071b8c13733df36c835fe81074c95f
Julian Grinblat <julian@dotcore.co.il>
2026-02-24 15:01:31 +0900

    fix: formalize pass and fail chars

--- mk/tests.mk
+++ mk/tests.mk
@@ -1,16 +1,19 @@
+FAIL := ❌
+PASS := ✅
+
 define test_targets
 @missing=0; \
 $(foreach t,$(1), \
 	if [ -z "$($(t)_target)" ]; then \
-		echo "Missing target variable: $(t)_target"; \
+		echo "$(FAIL) Missing target variable: $(t)_target"; \
 		missing=1; \
 	fi; \
 	for f in $(call escape_par,$($(t)_target)); do \
 		if [ ! -e "$$f" ]; then \
-			echo "Missing: $(t) - $$f"; \
+			echo "$(FAIL) Missing: $(t) - $$f"; \
 			missing=1; \
 		else \
-			echo "Found: $(t) - $$f"; \
+			echo "$(PASS) Found: $(t) - $$f"; \
 		fi; \
 	done; ) \
 exit $$missing
@@ -35,30 +38,30 @@ check-submodule-mismatch:
 		fi; \
 	done < <(git config --file .gitmodules --get-regexp '\.path'); \
 	if [ "$$mismatch" -ne 0 ]; then exit 1; fi
-	@echo "✓ no .gitmodules path mismatch"
+	@echo "$(PASS) no .gitmodules path mismatch"
 
 .PHONY: test-ibus-config
 test-ibus-config:
 	@dconf dump /desktop/ibus/ | cmp -s - ${DCONF}/ibus.dconf
 	@dconf dump /org/freedesktop/ibus/ | cmp -s - ${DCONF}/ibus-engine.dconf
-	@echo "✓ ibus configuration present"
+	@echo "$(PASS) ibus configuration present"
 
 test-cron:
 	@if ! crontab -l 2>/dev/null | cmp -s - $(CRONTAB_SRC); then \
-		echo "❌ cron contents do not match"; \
+		echo "$(FAIL) cron contents do not match"; \
 		exit 1; \
 	fi
-	@echo "✓ crontab matches"
+	@echo "$(PASS) crontab matches"
 
 .PHONY: test-dirs
 test-dirs:
 	@missing=0; \
 	for dir in $(dirs); do \
 		if [ ! -d "$$dir" ]; then \
-			echo "❌ dir $$dir does not exist or is not a directory"; \
+			echo "$(FAIL) dir $$dir does not exist or is not a directory"; \
 			missing=1; \
 		else \
-			echo "✅ dir $$dir exists"; \
+			echo "$(PASS) dir $$dir exists"; \
 		fi; \
 	done; \
 	exit $$missing
@@ -66,10 +69,10 @@ test-dirs:
 .PHONY: test-fonts
 test-fonts:
 	@fc-list | grep -qi Iosevka.ttc || { \
-		echo "❌ Iosevka not found in fontconfig cache"; \
+		echo "$(FAIL) Iosevka not found in fontconfig cache"; \
 		exit 1; \
 	}
-	@echo "✓ Iosevka present"
+	@echo "$(PASS) Iosevka present"
 
 .PHONY: test-neovim
 test-neovim:
@@ -78,7 +81,7 @@ test-neovim:
 	if [ -n "$$err" ]; then \
 	    echo "$$err"; exit 1; \
 	else \
-	    echo "✓ neovim boots cleanly"; \
+	    echo "$(PASS) neovim boots cleanly"; \
 	fi
 
 .PHONY: test

9cedacedc6cbe8e767ebb1dace7c6976ebb9f0bf
Julian Grinblat <julian@dotcore.co.il>
2026-02-25 03:26:35 +0900

    feat(neovim): replace wf.nvim with which-key.nvim

---
+++ home/.config/nvim/plugin/wk.lua
@@ -0,0 +1,33 @@
+local defer = require('defer')
+
+defer.on_load('which-key', function()
+	local wk = require('which-key')
+	wk.setup()
+	wk.add({
+		{ '<leader>b', group = 'buffers' },
+		-- { '<leader>c', group = 'osc52' },
+		{ '<leader>d', group = 'dap' },
+		{ '<leader>e', group = 'execute' }, -- only lua and vim files
+		{ '<leader>g', group = 'fugitive' },
+		{ '<leader>h', group = 'hunks' },
+		{ '<leader>n', group = 'neotree' },
+		{ '<leader>t', group = 'test' },
+		{ '<leader>q', group = 'persistence' },
+		{ '<leader>.', group = 'execute' },
+		{ '\\', group = 'toggle' },
+		{ require('pickers').prefix, group = 'telescope' },
+	})
+end)
+
+defer.pack('which-key', 'which-key.nvim')
+defer.very_lazy('which-key')
+local with = defer.with('which-key')
+
+require('map').map(
+	'n',
+	'<leader>?',
+	with(function(wk)
+		wk.show({ global = false })
+	end),
+	'Buffer Local Keymaps (which-key)'
+)

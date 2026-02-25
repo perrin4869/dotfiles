7c6dcb568a0529172aa5eb4d1e78e37915acacc3
Julian Grinblat <julian@dotcore.co.il>
2026-02-25 18:24:40 +0900

    feat(neovim): add wrapped.nvim

---
+++ home/.config/nvim/plugin/wrapped.lua
@@ -0,0 +1,6 @@
+local defer = require('defer')
+defer.pack('wrapped', 'wrapped.nvim')
+defer.cmd('WrappedNvim', 'wrapped')
+defer.cmd('NvimWrapped', 'wrapped')
+
+require('map').map('n', '<leader>W', defer.with('wrapped')(defer.call('run')), 'wrapped')

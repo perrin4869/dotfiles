3c4ab2879dbbb7447558af11c771af4494665b3a
Julian Grinblat <julian@dotcore.co.il>
2026-02-27 14:43:41 +0900

    feat(neovim): neotree avante integration

--- home/.config/nvim/plugin/tree.lua
+++ home/.config/nvim/plugin/tree.lua
@@ -1,7 +1,38 @@
 local defer = require('defer')
 
 defer.on_load('neo-tree', function()
-	require('neo-tree').setup({})
+	require('neo-tree').setup({
+		filesystem = {
+			commands = {
+				avante_add_files = defer.with('avante')(function(state)
+					local node = state.tree:get_node()
+					local filepath = node:get_id()
+					local relative_path = require('avante.utils').relative_path(filepath)
+
+					local sidebar = require('avante').get()
+
+					local open = sidebar:is_open()
+					-- ensure avante sidebar is open
+					if not open then
+						require('avante.api').ask()
+						sidebar = require('avante').get()
+					end
+
+					sidebar.file_selector:add_selected_file(relative_path)
+
+					-- remove neo tree buffer
+					if not open then
+						sidebar.file_selector:remove_selected_file('neo-tree filesystem [1]')
+					end
+				end),
+			},
+			window = {
+				mappings = {
+					['oa'] = 'avante_add_files',
+				},
+			},
+		},
+	})
 end)
 defer.pack('neo-tree', 'neo-tree.nvim')
 defer.cmd('Neotree', 'neo-tree')
